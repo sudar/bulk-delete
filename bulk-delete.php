cf9b90e44874e1113b165601313de8f5f814cfd5
sudar <sudar@sudarmuthu.com>
2009-07-14 13:30:35 +0000

    v0.4 - Added filter by date
    git-svn-id: http://plugins.svn.wordpress.org/bulk-delete/trunk@135031 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bulk-delete.php
+++ bulk-delete.php
@@ -4,7 +4,7 @@ Plugin Name: Bulk Delete
 Plugin Script: bulk-delete.php
 Plugin URI: http://sudarmuthu.com/wordpress/bulk-delete
 Description: Bulk delete posts from selected categories or tags. Use it with caution.
-Version: 0.3
+Version: 0.4
 License: GPL
 Author: Sudar
 Author URI: http://sudarmuthu.com/
@@ -13,6 +13,7 @@ Author URI: http://sudarmuthu.com/
 2009-02-02 - v0.1 - first version
 2009-02-03 - v0.2 - Second release - Fixed issues with pagging
 2009-04-05 - v0.3 - Third release - Prevented drafts from deleted when only posts are selected
+2009-07-05 - v0.4 - Fourth release - Added option to delete by date.
 
 */
 
@@ -24,6 +25,7 @@ Author URI: http://sudarmuthu.com/
 if (!function_exists('smbd_request_handler')) {
     function smbd_request_handler() {
         global $wpdb;
+
         if (isset($_POST['smbd_action'])) {
 
             $wp_query = new WP_Query;
@@ -34,8 +36,14 @@ if (!function_exists('smbd_request_handler')) {
                 case "bulk-delete-cats":
                     // delete by cats
                     $selected_cats = $_POST['smbd_cats'];
-                    $posts = $wp_query->query(array('category__in'=>$selected_cats,'post_status'=>'publish', 'post_type'=>'post', 'nopaging'=>'true'));
+                    if ($_POST['smbd_cats_restrict'] == "true") {
 
+                        add_option('cats_op', $_POST['smbd_cats_op']);
+                        add_option('cats_days', $_POST['smbd_cats_days']);
+                        
+                        add_filter ('posts_where', 'cats_by_days');
+                    }
+                    $posts = $wp_query->query(array('category__in'=>$selected_cats,'post_status'=>'publish', 'post_type'=>'post', 'nopaging'=>'true'));
                     foreach ($posts as $post) {
                         wp_delete_post($post->ID);
                     }
@@ -45,6 +53,13 @@ if (!function_exists('smbd_request_handler')) {
                 case "bulk-delete-tags":
                     // delete by tags
                     $selected_tags = $_POST['smbd_tags'];
+                    if ($_POST['smbd_tags_restrict'] == "true") {
+
+                        add_option('tags_op', $_POST['smbd_tags_op']);
+                        add_option('tags_days', $_POST['smbd_tags_days']);
+
+                        add_filter ('posts_where', 'tags_by_days');
+                    }
                     $posts = $wp_query->query(array('tag__in'=>$selected_tags, 'post_status'=>'publish', 'post_type'=>'post', 'nopaging'=>'true'));
 
                     foreach ($posts as $post) {
@@ -104,10 +119,10 @@ if (!function_exists('smbd_displayOptions')) {
         global $wpdb;
 ?>
 	<div class="updated fade" style="background:#ff0;text-align:center;color: red;"><p><strong><?php _e("WARNING: Posts deleted once cannot be retrieved back. Use with caution."); ?></strong></p></div>
-    <div class="wrap">
-		<h2>Bulk Delete</h2>
+        <div class="wrap">
+            <h2>Bulk Delete</h2>
 
-        <h3><?php _e("Select the posts which you want to delete"); ?></h3>
+            <h3><?php _e("Select the posts which you want to delete"); ?></h3>
 
         <form name="smbd_form" id = "smbd_misc_form"
         action="<?php echo get_bloginfo("wpurl"); ?>/wp-admin/options-general.php?page=bulk-delete.php" method="post"
@@ -120,7 +135,7 @@ if (!function_exists('smbd_displayOptions')) {
         $pages = $wp_query->query(array('post_type'=>'page'));
 ?>
         <fieldset class="options">
-		<table class="optiontable">
+        <table class="optiontable">
             <tr>
                 <td scope="row" >
                     <input name="smbd_drafs" id ="smbd_drafs" value = "drafs" type = "checkbox" />
@@ -142,16 +157,16 @@ if (!function_exists('smbd_displayOptions')) {
                 </td>
             </tr>
 
-		</table>
-		</fieldset>
+        </table>
+        </fieldset>
         <p class="submit">
-				<input type="submit" name="submit" value="<?php _e("Bulk Delete ") ?>&raquo;">
+                <input type="submit" name="submit" value="<?php _e("Bulk Delete ") ?>&raquo;">
         </p>
 
-<?php wp_nonce_field('bulk-delete-posts'); ?>
+        <?php wp_nonce_field('bulk-delete-posts'); ?>
 
-		<input type="hidden" name="smbd_action" value="bulk-delete-special" />
-		</form>
+        <input type="hidden" name="smbd_action" value="bulk-delete-special" />
+        </form>
 
         <h3><?php _e("By Category"); ?></h3>
         <h4><?php _e("Select the categories whose post you want to delete") ?></h4>
@@ -161,7 +176,7 @@ if (!function_exists('smbd_displayOptions')) {
         onsubmit="return bd_validateForm(this);">
 
         <fieldset class="options">
-		<table class="optiontable">
+            <table class="optiontable">
 <?php
         $categories =  get_categories(array('hide_empty' => false));
         foreach ($categories as $category) {
@@ -185,9 +200,26 @@ if (!function_exists('smbd_displayOptions')) {
                     <label for="smbd_cats_all"><?php _e("All Categories") ?></label>
                 </td>
             </tr>
+            <tr>
+                <td colspan="2"></td>
+            </tr>
 
-		</table>
-		</fieldset>
+            <tr>
+                <td scope="row">
+                    <input name="smbd_cats_restrict" id="smbd_cats_restrict" value = "true" type = "checkbox"  onclick="toggle_date_restrict('cats');" />
+                </td>
+                <td>
+                    <?php _e("Only restrict to posts which are ");?>
+                    <select name="smbd_cats_op" id="smbd_cats_op" disabled>
+                        <option value ="<"><?php _e("older than");?></option>
+                        <option value =">"><?php _e("posted within last");?></option>
+                    </select>
+                    <input type ="textbox" name="smbd_cats_days" id="smbd_cats_days" disabled value ="0" maxlength="4" size="4" /><?php _e("days");?>
+                </td>
+            </tr>
+
+        </table>
+        </fieldset>
         <p class="submit">
 				<input type="submit" name="submit" value="<?php _e("Bulk Delete ") ?>&raquo;">
         </p>
@@ -229,6 +261,24 @@ if (!function_exists('smbd_displayOptions')) {
                     </td>
                     <td>
                         <label for="smbd_tags_all"><?php _e("All Tags") ?></label>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td colspan="2"></td>
+                </tr>
+
+                <tr>
+                    <td scope="row">
+                        <input name="smbd_tags_restrict" id ="smbd_tags_restrict" value = "true" type = "checkbox" onclick="toggle_date_restrict('tags');" />
+                    </td>
+                    <td>
+                        <?php _e("Only restrict to posts which are ");?>
+                        <select name="smbd_tags_op" id="smbd_tags_op" disabled>
+                            <option value ="<"><?php _e("older than");?></option>
+                            <option value =">"><?php _e("posted within last");?></option>
+                        </select>
+                        <input type ="textbox" name="smbd_tags_days" id ="smbd_tags_days" value ="0"  maxlength="4" size="4" disabled /><?php _e("days");?>
                     </td>
                 </tr>
 
@@ -275,6 +325,15 @@ function smbd_print_scripts() {
         }
     }
 
+    function toggle_date_restrict(el) {
+        if (jQuery("#smbd_" + el + "_restrict").is(":checked")) {
+            jQuery("#smbd_" + el + "_op").removeAttr('disabled');
+            jQuery("#smbd_" + el + "_days").removeAttr('disabled');
+        } else {
+            jQuery("#smbd_" + el + "_op").attr('disabled', 'true');
+            jQuery("#smbd_" + el + "_days").attr('disabled', 'true');
+        }
+    }
     /**
      * Validate Form
      */
@@ -298,6 +357,34 @@ function smbd_print_scripts() {
     }
 </script>
 <?php
+}
+
+/**
+ * function to filter posts by days
+ * @param <type> $where
+ * @return <type>
+ */
+function cats_by_days ($where = '') {
+    $cats_op = get_option('cats_op');
+    $cats_days = get_option('cats_days');
+    remove_filter('posts_where', 'cats_by_days');
+
+    $where .= " AND post_date $cats_op '" . date('y-m-d', strtotime("-$cats_days days")) . "'";
+    return $where;
+}
+
+/**
+ * function to filter posts by days
+ * @param <type> $where
+ * @return <type>
+ */
+function tags_by_days ($where = '') {
+    $tags_op = get_option('tags_op');
+    $tags_days = get_option('tags_days');
+    
+    remove_filter('posts_where', 'tags_by_days');
+    $where .= " AND post_date $tags_op '" . date('y-m-d', strtotime("-$tags_days days")) . "'";
+    return $where;
 }
 
 /**
@@ -331,7 +418,7 @@ function smbd_filter_plugin_actions($links, $file) {
  * Adds Footer links. Based on http://striderweb.com/nerdaphernalia/2008/06/give-your-wordpress-plugin-credit/
  */
 function smbd_admin_footer() {
-	$plugin_data = get_plugin_data( __FILE__ );
+    $plugin_data = get_plugin_data( __FILE__ );
     printf('%1$s ' . __("plugin") .' | ' . __("Version") . ' %2$s | '. __('by') . ' %3$s<br />', $plugin_data['Title'], $plugin_data['Version'], $plugin_data['Author']);
 }
 
--- readme.txt
+++ readme.txt
@@ -1,9 +1,9 @@
 === Bulk Delete ===
 Contributors: sudar 
 Tags: post, comment, delete, bulk, draft, revision, page
-Requires at least: 2.5
-Tested up to: 2.7.1
-Stable tag: 0.3
+Requires at least: 2.0
+Tested up to: 2.8.1
+Stable tag: 0.4
 	
 Bulk delete posts from selected categories or tags
 
@@ -22,6 +22,24 @@ If you looking for just moving posts, instead of deleting, then use [Bulk Move P
 
 Extract the zip file and just drop the contents in the wp-content/plugins/ directory of your WordPress installation and then activate the Plugin from Plugins page.
 
- ==Readme Generator== 
+== Changelog ==
 
-This Readme file was generated using <a href = 'http://sudarmuthu.com/projects/wp-readme/'>wp-readme</a>, which generates readme files for WordPress Plugins.
+**v0.1 (2009-02-02)**  
+
+*   first version
+
+**v0.2 (2009-02-03)**
+
+*   Fixed issues with pagging
+
+**v0.3 (2009-04-05)**
+
+*   Prevented drafts from deleted when only posts are selected
+
+**v0.4 (2009-07-05)**
+
+*   Added option to delete by date.
+
+==Readme Generator== 
+
+This Readme file was generated using <a href = 'http://sudarmuthu.com/projects/wp-readme.php'>wp-readme</a>, which generates readme files for WordPress Plugins.
