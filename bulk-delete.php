5f91af4f95159f7d1d4fa5e1c825c2a275a6a646
Sudar <sudar@sudarmuthu.com>
2013-02-16 17:12:42 +0530

    Replaced hardcoded path with WPINC constant

--- bulk-delete.php
+++ bulk-delete.php
@@ -635,7 +635,7 @@ class Bulk_Delete {
     function display_cron_page() {
         
         if(!class_exists('WP_List_Table')){
-            require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
+            require_once( ABSPATH . WPINC . '/class-wp-list-table.php' );
         }
         if (!class_exists('Cron_List_Table')) {
             require_once dirname(__FILE__) . '/include/class-cron-list-table.php';
