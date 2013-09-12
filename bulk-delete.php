sudar/bulk-delete

commit ac3cc7d5ab0f6ca9fc98beff1b3a0602044bb4d9

Add the ability to schedule auto delete of users by user role
Fix #16

bulk-delete.php
@@ -3,7 +3,7 @@
 Plugin Name: Bulk Delete
 Plugin Script: bulk-delete.php
 Plugin URI: http://sudarmuthu.com/wordpress/bulk-delete
-Description: Bulk delete posts from selected categories, tags, post types, custom taxonomies or by post status like drafts, scheduled posts, revisions etc.
+Description: Bulk delete users and posts from selected categories, tags, post types, custom taxonomies or by post status like drafts, scheduled posts, revisions etc.
 Donate Link: http://sudarmuthu.com/if-you-wanna-thank-me
 Version: 4.0.0
 License: GPL
@@ -126,6 +126,8 @@ class Bulk_Delete {
     const CRON_HOOK_TAGS        = 'do-bulk-delete-tags';
     const CRON_HOOK_TAXONOMY    = 'do-bulk-delete-taxonomy';
     const CRON_HOOK_POST_TYPES  = 'do-bulk-delete-post-types';
+
+    const CRON_HOOK_USER_ROLE   = 'do-bulk-delete-users-by-role';
 
     // meta boxes for delete posts
     const BOX_POST_STATUS       = 'bd_by_post_status';
@@ -1338,10 +1340,10 @@ class Bulk_Delete {
 
             <tr>
                 <td scope="row" colspan="2">
-                    <input name="smbd_cats_cron" value = "false" type = "radio" checked="checked" /> <?php _e('Delete now', 'bulk-delete'); ?>
-                    <input name="smbd_cats_cron" value = "true" type = "radio" id = "smbd_cats_cron" disabled > <?php _e('Schedule', 'bulk-delete'); ?>
-                    <input name="smbd_cats_cron_start" id = "smbd_cats_cron_start" value = "now" type = "text" disabled><?php _e('repeat ', 'bulk-delete');?>
-                    <select name = "smbd_cats_cron_freq" id = "smbd_cats_cron_freq" disabled>
+                    <input name="smbdu_userrole_cron" value = "false" type = "radio" checked="checked" /> <?php _e('Delete now', 'bulk-delete'); ?>
+                    <input name="smbdu_userrole_cron" value = "true" type = "radio" id = "smbdu_userrole_cron" disabled > <?php _e('Schedule', 'bulk-delete'); ?>
+                    <input name="smbdu_userrole_cron_start" id = "smbdu_userrole_cron_start" value = "now" type = "text" disabled><?php _e('repeat ', 'bulk-delete');?>
+                    <select name = "smbdu_userrole_cron_freq" id = "smbdu_userrole_cron_freq" disabled>
                         <option value = "-1"><?php _e("Don't repeat", 'bulk-delete'); ?></option>
 <?php
         $schedules = wp_get_schedules();
@@ -1352,7 +1354,7 @@ class Bulk_Delete {
         }
 ?>
                     </select>
-                    <span class = "bd-cats-pro" style = "color:red"><?php _e('Only available in Pro Addon', 'bulk-delete'); ?> <a href = "http://sudarmuthu.com/out/buy-bulk-delete-category-addon">Buy now</a></span>
+                    <span class = "bdu-users-by-role-pro" style = "color:red"><?php _e('Only available in Pro Addon', 'bulk-delete'); ?> <a href = "http://sudarmuthu.com/out/buy-bulk-delete-users-by-role-addon">Buy now</a></span>
                 </td>
             </tr>
 
@@ -1479,14 +1481,28 @@ class Bulk_Delete {
 
                     $delete_options = array();
                     $delete_options['selected_roles']   = array_get( $_POST, 'smbdu_roles' );
-                    $delete_options['no_posts']         = array_get( $_POST, 'smbdu_role_no_posts' );
+                    $delete_options['no_posts']         = array_get( $_POST, 'smbdu_role_no_posts', FALSE );
 
                     $delete_options['login_restrict']   = array_get( $_POST, 'smbdu_login_restrict', FALSE );
                     $delete_options['login_days']       = array_get( $_POST, 'smbdu_login_days' );
                     $delete_options['limit_to']         = array_get( $_POST, 'smbdu_role_limit' );
 
-                    $deleted_count = Bulk_Delete_Users::delete_users_by_role( $delete_options );
-                    $this->msg = sprintf( _n('Deleted %d user from the selected roles', 'Deleted %d users from the selected role' , $deleted_count, 'bulk-delete' ), $deleted_count );
+                    if (array_get( $_POST, 'smbdu_userrole_cron', 'false' ) == 'true' ) {
+                        $freq = $_POST['smbdu_userrole_cron_freq'];
+                        $time = strtotime( $_POST['smbdu_userrole_cron_start'] ) - ( get_option( 'gmt_offset' ) * 60 * 60 );
+
+                        if ( $freq == -1 ) {
+                            wp_schedule_single_event( $time, self::CRON_HOOK_USER_ROLE, array( $delete_options ) );
+                        } else {
+                            wp_schedule_event( $time, $freq , self::CRON_HOOK_USER_ROLE, array( $delete_options ) );
+                        }
+
+                        $this->msg = __( 'Users from the selected userrole are scheduled for deletion.', 'bulk-delete' ) . ' ' . 
+                            sprintf( __( 'See the full list of <a href = "%s">scheduled tasks</a>' , 'bulk-delete' ), get_bloginfo( "wpurl" ) . '/wp-admin/options-general.php?page=bulk-delete-cron' );
+                    } else {
+                        $deleted_count = Bulk_Delete_Users::delete_users_by_role( $delete_options );
+                        $this->msg = sprintf( _n('Deleted %d user from the selected roles', 'Deleted %d users from the selected role' , $deleted_count, 'bulk-delete' ), $deleted_count );
+                    }
 
                     break;
             }

js/bulk-delete.js
@@ -47,17 +47,17 @@ jQuery(document).ready(function () {
     // for post boxes
     postboxes.add_postbox_toggles(pagenow);
 
-    jQuery.each(['cats', 'tags', 'taxs', 'pages', 'post_status', 'types'], function (index, value) {
+    jQuery.each(['_cats', '_tags', '_taxs', '_pages', '_post_status', '_types', 'u_userrole'], function (index, value) {
         // invoke the date time picker
-        jQuery('#smbd_' + value + '_cron_start').datetimepicker({
+        jQuery('#smbd' + value + '_cron_start').datetimepicker({
             timeFormat: 'HH:mm:ss'
         });
 
-        jQuery('#smbd_' + value + '_restrict').change(function () {
+        jQuery('#smbd' + value + '_restrict').change(function () {
             toggle_date_restrict(value);
         });
 
-        jQuery('#smbd_' + value + '_limit').change(function () {
+        jQuery('#smbd' + value + '_limit').change(function () {
             toggle_limit_restrict(value);
         });
 

include/class-bulk-delete-users.php
@@ -12,8 +12,8 @@ class Bulk_Delete_Users {
      */
     public static function delete_users_by_role( $delete_options ) {
 
-        if( !function_exists( 'wp_delete_users' ) ) {
-            require_once( ABSPATH . WPINC . '/user.php' );
+        if( !function_exists( 'wp_delete_user' ) ) {
+            require_once( ABSPATH . 'wp-admin/includes/user.php' );
         }
 
         $count = 0;

commit 2d49e0d3aa6e4bf133202ce4f951fb7b5f0b6bf5

Fix JavaScript bug that prevented deleting posts by days and in batches
This was introduced in commit ac3cc7d5ab0f6ca9fc98beff1b3a0602044bb4d9

bulk-delete.php
@@ -5,7 +5,7 @@ Plugin Script: bulk-delete.php
 Plugin URI: http://sudarmuthu.com/wordpress/bulk-delete
 Description: Bulk delete users and posts from selected categories, tags, post types, custom taxonomies or by post status like drafts, scheduled posts, revisions etc.
 Donate Link: http://sudarmuthu.com/if-you-wanna-thank-me
-Version: 4.0
+Version: 4.0.1
 License: GPL
 Author: Sudar
 Author URI: http://sudarmuthu.com/
@@ -80,6 +80,8 @@ Domain Path: languages/
 2013-09-09 - v4.0 - (Dev time: 25 hours)
                   - Add the ability to delete users
                   - Move menu items under tools
+2013-09-12 - v4.0.1 - (Dev time: 1 hours)
+                  - Fix JavaScript bug that prevented deleting posts by days and in batches
 */
 
 /*  Copyright 2009  Sudar Muthu  (email : sudar@sudarmuthu.com)
@@ -111,7 +113,7 @@ if ( !class_exists( 'Bulk_Delete_Util' ) ) {
  */
 class Bulk_Delete {
     
-    const VERSION               = '4.0';
+    const VERSION               = '4.0.1';
 
     // page slugs
     const USERS_PAGE_SLUG       = 'bulk-delete-users';

js/bulk-delete.js
@@ -24,12 +24,12 @@ jQuery(document).ready(function () {
      * Toggle the date restrict fields
      */
     function toggle_date_restrict(el) {
-        if (jQuery("#smbd_" + el + "_restrict").is(":checked")) {
-            jQuery("#smbd_" + el + "_op").removeAttr('disabled');
-            jQuery("#smbd_" + el + "_days").removeAttr('disabled');
+        if (jQuery("#smbd" + el + "_restrict").is(":checked")) {
+            jQuery("#smbd" + el + "_op").removeAttr('disabled');
+            jQuery("#smbd" + el + "_days").removeAttr('disabled');
         } else {
-            jQuery("#smbd_" + el + "_op").attr('disabled', 'true');
-            jQuery("#smbd_" + el + "_days").attr('disabled', 'true');
+            jQuery("#smbd" + el + "_op").attr('disabled', 'true');
+            jQuery("#smbd" + el + "_days").attr('disabled', 'true');
         }
     }
 
@@ -37,10 +37,10 @@ jQuery(document).ready(function () {
      * Toggle limit restrict fields
      */
     function toggle_limit_restrict(el) {
-        if (jQuery("#smbd_" + el + "_limit").is(":checked")) {
-            jQuery("#smbd_" + el + "_limit_to").removeAttr('disabled');
+        if (jQuery("#smbd" + el + "_limit").is(":checked")) {
+            jQuery("#smbd" + el + "_limit_to").removeAttr('disabled');
         } else {
-            jQuery("#smbd_" + el + "_limit_to").attr('disabled', 'true');
+            jQuery("#smbd" + el + "_limit_to").attr('disabled', 'true');
         }
     }
 