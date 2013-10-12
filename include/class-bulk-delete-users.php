64508a02c6c8465ae98a38371b41ecfabe87b498
Sudar <sudar@sudarmuthu.com>
2013-10-12 12:45:09 +0530

    Move static functions to separate classes
    Fix #18

--- include/class-bulk-delete-users.php
+++ include/class-bulk-delete-users.php
@@ -8,6 +8,113 @@
 class Bulk_Delete_Users {
 
     /**
+     * Render delete users box
+     */
+    function render_delete_users_box() {
+
+        if ( Bulk_Delete_Util::is_users_box_hidden( Bulk_Delete::BOX_USERS ) ) {
+            printf( __('This section just got enabled. Kindly <a href = "%1$s">refresh</a> the page to fully enable it.', 'bulk-delete' ), 'tools.php?page=' . Bulk_Delete::USERS_PAGE_SLUG );
+            return;
+        }
+?>
+        <!-- Users Start-->
+        <h4><?php _e("Select the user roles from which you want to delete users", 'bulk-delete'); ?></h4>
+
+        <fieldset class="options">
+        <table class="optiontable">
+<?php
+        $users_count = count_users();
+        foreach( $users_count['avail_roles'] as $role => $count ) {
+?>
+            <tr>
+                <td scope="row" >
+                    <input name="smbdu_roles[]" value = "<?php echo $role; ?>" type = "checkbox" />
+                </td>
+                <td>
+                    <label for="smbdu_roles"><?php echo $role; ?> (<?php echo $count . " "; _e("Users", 'bulk-delete'); ?>)</label>
+                </td>
+            </tr>
+<?php
+        }
+?>
+            <tr>
+                <td colspan="2">
+                    <h4><?php _e("Choose your filtering options", 'bulk-delete'); ?></h4>
+                </td>
+            </tr>
+
+<?php 
+        if ( !Bulk_Delete_Util::is_simple_login_log_present() ) {
+            $disabled = "disabled";
+        } else {
+            $disabled = '';
+        }
+?>
+            <tr>
+                <td scope="row">
+                <input name="smbdu_login_restrict" id="smbdu_login_restrict" value = "true" type = "checkbox" <?php echo $disabled; ?> >
+                </td>
+                <td>
+                    <?php _e("Only restrict to users who have not logged in the last ", 'bulk-delete');?>
+                    <input type ="textbox" name="smbdu_login_days" id="smbdu_login_days" value ="0" maxlength="4" size="4" <?php echo $disabled; ?> ><?php _e("days", 'bulk-delete');?>
+<?php 
+        if ( !Bulk_Delete_Util::is_simple_login_log_present() ) {
+?>
+                    <span style = "color:red">
+                        <?php _e('Need Simple Login Log Plugin', 'bulk-delete'); ?> <a href = "http://wordpress.org/plugins/simple-login-log/">Install now</a>
+                    </span>
+<?php
+        }
+?>
+                </td>
+            </tr>
+
+            <tr>
+                <td scope="row">
+                    <input name="smbdu_role_no_posts" id="smbdu_role_no_posts" value = "true" type = "checkbox" >
+                </td>
+                <td>
+                    <?php _e( "Only if user doesn't have any post", 'bulk-delete' ); ?>
+                </td>
+            </tr>
+
+            <tr>
+                <td scope="row" colspan="2">
+                    <input name="smbdu_userrole_cron" value = "false" type = "radio" checked="checked" /> <?php _e('Delete now', 'bulk-delete'); ?>
+                    <input name="smbdu_userrole_cron" value = "true" type = "radio" id = "smbdu_userrole_cron" disabled > <?php _e('Schedule', 'bulk-delete'); ?>
+                    <input name="smbdu_userrole_cron_start" id = "smbdu_userrole_cron_start" value = "now" type = "text" disabled><?php _e('repeat ', 'bulk-delete');?>
+                    <select name = "smbdu_userrole_cron_freq" id = "smbdu_userrole_cron_freq" disabled>
+                        <option value = "-1"><?php _e("Don't repeat", 'bulk-delete'); ?></option>
+<?php
+        $schedules = wp_get_schedules();
+        foreach($schedules as $key => $value) {
+?>
+                        <option value = "<?php echo $key; ?>"><?php echo $value['display']; ?></option>
+<?php                        
+        }
+?>
+                    </select>
+                    <span class = "bdu-users-by-role-pro" style = "color:red"><?php _e('Only available in Pro Addon', 'bulk-delete'); ?> <a href = "http://sudarmuthu.com/out/buy-bulk-delete-users-by-role-addon">Buy now</a></span>
+                </td>
+            </tr>
+
+            <tr>
+                <td scope="row" colspan="2">
+                    <?php _e("Enter time in Y-m-d H:i:s format or enter now to use current time", 'bulk-delete');?>
+                </td>
+            </tr>
+
+        </table>
+        </fieldset>
+
+        <p>
+            <button type="submit" name="smbdu_action" value = "bulk-delete-users-by-role" class="button-primary"><?php _e("Bulk Delete ", 'bulk-delete') ?>&raquo;</button>
+        </p>
+        <!-- Users end-->
+<?php
+    }
+
+    /**
      * Delete users by user role
      */
     public static function delete_users_by_role( $delete_options ) {
--- include/class-bulk-delete-util.php
+++ include/class-bulk-delete-util.php
@@ -10,6 +10,10 @@ class Bulk_Delete_Util {
     // simple login log
     const SIMPLE_LOGIN_LOG_TABLE = 'simple_login_log';
 
+    // Meta boxes
+    const VISIBLE_POST_BOXES     = 'metaboxhidden_tools_page_bulk-delete';
+    const VISIBLE_USER_BOXES     = 'metaboxhidden_tools_page_bulk-delete-users';
+
     /**
      * Find out if Simple Login Log is installed or not
      * http://wordpress.org/plugins/simple-login-log/
@@ -22,6 +26,84 @@ class Bulk_Delete_Util {
         }
 
         return FALSE;
+    }
+
+    /**
+     * @brief Check whether the meta box in posts page is hidden or not
+     *
+     * @param $box
+     *
+     * @return 
+     */
+    public static function is_posts_box_hidden( $box ) {
+        $hidden_boxes = self::get_posts_hidden_boxes();
+        return ( is_array( $hidden_boxes ) && in_array( $box, $hidden_boxes ) );
+    }
+
+    /**
+     * Get the list of hidden boxes in posts page
+     *
+     * @return the array of hidden meta boxes
+     */
+    public static function get_posts_hidden_boxes() {
+        $current_user = wp_get_current_user();
+        return get_user_meta( $current_user->ID, self::VISIBLE_POST_BOXES, TRUE );
+    }
+
+    /**
+     * @brief Check whether the meta box in users page is hidden or not
+     *
+     * @param $box
+     *
+     * @return 
+     */
+    public static function is_users_box_hidden( $box ) {
+        $hidden_boxes = self::get_users_hidden_boxes();
+        return ( is_array( $hidden_boxes ) && in_array( $box, $hidden_boxes ) );
+    }
+
+    /**
+     * Get the list of hidden boxes in users page
+     *
+     * @return the array of hidden meta boxes
+     */
+    public static function get_users_hidden_boxes() {
+        $current_user = wp_get_current_user();
+        return get_user_meta( $current_user->ID, self::VISIBLE_USER_BOXES, TRUE );
+    }
+
+    /**
+     * Get the list of cron schedules
+     *
+     * @return array - The list of cron schedules
+     */
+    public static function get_cron_schedules() {
+
+        $cron_items = array();
+		$cron = _get_cron_array();
+		$date_format = _x( 'M j, Y @ G:i', 'Cron table date format', 'bulk-delete' );
+        $i = 0;
+
+		foreach ( $cron as $timestamp => $cronhooks ) {
+			foreach ( (array) $cronhooks as $hook => $events ) {
+                if (substr($hook, 0, 15) == 'do-bulk-delete-') {
+                    $cron_item = array();
+
+                    foreach ( (array) $events as $key => $event ) {
+                        $cron_item['timestamp'] = $timestamp;
+                        $cron_item['due'] = date_i18n( $date_format, $timestamp + ( get_option('gmt_offset') * 60 * 60 ) );
+                        $cron_item['schedule'] = $event['schedule'];
+                        $cron_item['type'] = $hook;
+                        $cron_item['args'] = $event['args'];
+                        $cron_item['id'] = $i;
+                    }
+
+                    $cron_items[$i] = $cron_item;
+                    $i++;
+                }
+            }
+        }
+        return $cron_items;
     }
 }
 ?>
