d9c1c71ec167d9cd79222990fc30d373c26300b1
Sudar <sudar@sudarmuthu.com>
2015-05-16 17:08:47 +0530

    Don't validate JSDoc

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function( grunt ) {
 			},
 			grunt: {
 				files:{
-					src: [ 'Gruntfile.js' ] 
+					src: [ 'Gruntfile.js' ]
 				},
 				options: {
 					jshintrc: '.gruntjshintrc'
