e5ecdb201a696d03017ade818f2ab60642ef401a
Sudar <sudar@sudarmuthu.com>
2015-07-01 16:46:43 +0530

    Add the ability to add campaign params to the function that generates addon url

--- include/addons/util.php
+++ include/addons/util.php
@@ -42,18 +42,20 @@ function bd_get_addon_class_name( $addon_name ) {
  *
  * @since 5.5
  *
- * @param string $addon_name Name of the addon.
- * @return string Computed url for the addon.
+ * @param  string $addon_name    Name of the addon.
+ * @param  array  $campaign_args Campaign_args. Default empty array
+ * @return string                Computed url for the addon.
  */
-function bd_get_addon_url( $addon_name ) {
+function bd_get_addon_url( $addon_name, $campaign_args = array() ) {
 	$base = 'http://bulkwp.com/addons/';
-	$addon_slug = str_replace( ' ', '-', strtolower( $addon_slug ) );
+	$addon_slug = str_replace( ' ', '-', strtolower( $addon_name ) );
 
 	if ( false !== strpos( $addon_name, 'scheduler' ) ) {
 		$addon_slug = str_replace( 'bulk-delete-', '', $addon_name );
 	}
 
 	$addon_url = $base . $addon_slug;
+	$addon_url = add_query_arg( $campaign_args, $addon_url );
 
 	/**
 	 * Filter to modify addon url.
@@ -62,7 +64,8 @@ function bd_get_addon_url( $addon_name ) {
 	 *
 	 * @param string $addon_name Addon name
 	 * @param string $addon_url Addon url
+	 * @param array  $campaign_args Campaign_args. Default empty array
 	 */
-	return apply_filters( 'bd_addon_url', $addon_url, $addon_name );
+	return apply_filters( 'bd_addon_url', $addon_url, $addon_name, $campaign_args );
 }
 ?>
