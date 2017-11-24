0b26c449fa41c29a5b15f32d413ffdbc4a2cae21
Maria Daniel Deepak <mariadanieldeepak@gmail.com>
2017-11-24 23:24:58 +0530

    RC: Use single line PHPCS whitelist
    Review comment ref: https://github.com/sudar/bulk-delete/pull/148\#discussion_r152537067

--- include/users/modules/class-bulk-delete-users-by-user-role.php
+++ include/users/modules/class-bulk-delete-users-by-user-role.php
@@ -107,9 +107,7 @@ class Bulk_Delete_Users_By_User_Role extends BD_User_Meta_Box_Module {
 		/*
 		 * Nonce verification is handled in `request_handler()`.
 		 */
-		// @codingStandardsIgnoreStart
-		$delete_options['selected_roles'] = array_get( $_POST, 'smbd_u_roles' );
-		// @codingStandardsIgnoreEnd
+		$delete_options['selected_roles'] = array_get( $_POST, 'smbd_u_roles' ); // WPCS: XSS ok.
 
 		$this->process_user_delete( $delete_options );
 	}
