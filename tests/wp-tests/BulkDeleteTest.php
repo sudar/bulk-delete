d07d89a723ea2893560158218b199b5faa673f93
Sudar Muthu <sudar@sudarmuthu.com>
2017-12-31 09:08:09 +0530

    Setup WPCore tests
    Fix #136

---
+++ tests/wp-tests/BulkDeleteTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use BulkWP\Tests\WPCore\WPCoreTestCase;
+
+/**
+ * Sample test case.
+ */
+class BulkDeleteTest extends WPCoreTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
