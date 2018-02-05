1dcd97bc3574abe40b8c27d95454725f95417b0c
anandB000 <anandrajbalu00@gmail.com>
2018-02-05 21:11:13 +0530

    Add test for delete users by role with filter set registered day at least two days
    Fix #157

--- tests/wp-unit/tests/include/users/Bulk_Delete_Users_By_User_RoleTest.php
+++ tests/wp-unit/tests/include/users/Bulk_Delete_Users_By_User_RoleTest.php
@@ -33,9 +33,9 @@ class Bulk_Delete_Users_By_User_RoleTest extends WPCoreUnitTestCase {
 		// call our method.
 		$delete_options = array(
 			'selected_roles'      => array( 'subscriber' ),
-			'limit_to'            => 0,
-			'registered_restrict' => 0,
-			'login_restrict'      => 0,
+			'limit_to'            => false,
+			'registered_restrict' => false,
+			'login_restrict'      => false,
 			'no_posts'            => false,
 		);
 		$this->delete_by_user_role->delete( $delete_options );
@@ -44,5 +44,37 @@ class Bulk_Delete_Users_By_User_RoleTest extends WPCoreUnitTestCase {
 		$users_in_role = get_users( array( 'role' => 'subscriber' ) );
 
 		$this->assertEquals( 0, count( $users_in_role ) );
+	}
+
+	/**
+	 * Test case of delete users by role with filter set registered day at least two days.
+	 */
+	public function test_delete_users_by_role_with_filter_set_registered_day_at_least_two_days() {
+		//Set registered date
+		$day_past = date('Y-m-d', strtotime('-2 day'));
+
+		//Create one user and assign to subscriber role
+		$user = $this->factory->user->create( array( 'user_login' => 'user_test', 'user_pass' => 'ZXC987abc', 'role' => 'subscriber', 'user_registered' => $day_past ) );
+
+		// Assert that user role has one user.
+		$users_in_role = get_users( array( 'role' => 'subscriber' ) );
+
+		$this->assertEquals( 1, count( $users_in_role ) );
+
+		// call our method.
+		$delete_options = array(
+			'selected_roles'      => array( 'subscriber' ),
+			'limit_to'            => 0,
+			'registered_restrict' => true,
+			'registered_days'     => 3,
+			'login_restrict'      => 0,
+			'no_posts'            => false,
+		);
+		$this->delete_by_user_role->delete( $delete_options );
+
+		// Assert that user role has no user.
+		$users_in_role = get_users( array( 'role' => 'subscriber' ) );
+
+		$this->assertEquals( 1, count( $users_in_role ) );
 	}
 }
