bdc49187e3b721d426f0a8586513f245cc006bbf
anandB000 <anandrajbalu00@gmail.com>
2018-03-10 23:27:10 +0530

    Add test for delete user by meta with filters registered date by latest one day
    Fix #196

--- tests/wp-unit/tests/include/users/Bulk_Delete_Users_By_User_MetaTest.php
+++ tests/wp-unit/tests/include/users/Bulk_Delete_Users_By_User_MetaTest.php
@@ -78,4 +78,36 @@ class Bulk_Delete_Users_By_User_MetaTest extends WPCoreUnitTestCase {
 		$users_in_meta = get_users( array( 'meta_key' => 'first_name', 'meta_value' => 'bulk_delete', 'meta_compare' => '=' ) );
 		$this->assertEquals( 1, count( $users_in_meta ) );
 	}
+
+	/**
+	 * Test case of delete users by user meta with filter set registered day at least two days.
+	 */
+	public function test_delete_users_by_meta_with_filter_set_registered_day_at_least_one_day() {
+		//Set registered date
+		$day_past = date('Y-m-d', strtotime('-2 day'));
+
+		//Create one user and set meta and registered date
+		$user = $this->factory->user->create( array( 'user_login' => 'user_test', 'user_pass' => 'ZXC987abc', 'role' => 'subscriber', 'first_name' => 'bulk_delete', 'user_registered' => $day_past ) );
+
+		// Assert that user meta has one user.
+		$users_in_meta = get_users( array( 'meta_key' => 'first_name', 'meta_value' => 'bulk_delete', 'meta_compare' => '=' ) );
+		$this->assertEquals( 1, count( $users_in_meta ) );
+
+		// call our method.
+		$delete_options = array(
+			'meta_key'            => 'first_name',
+			'meta_value'          => 'bulk_delete',
+			'meta_compare'        => '=',
+			'limit_to'            => false,
+			'registered_restrict' => true,
+			'registered_days'     => 1,
+			'login_restrict'      => false,
+			'no_posts'            => false,
+		);
+		$this->delete_by_user_meta->delete( $delete_options );
+
+		// Assert that user meta has no user.
+		$users_in_meta = get_users( array( 'meta_key' => 'first_name', 'meta_value' => 'bulk_delete', 'meta_compare' => '=' ) );
+		$this->assertEquals( 0, count( $users_in_meta ) );
+	}
 }
