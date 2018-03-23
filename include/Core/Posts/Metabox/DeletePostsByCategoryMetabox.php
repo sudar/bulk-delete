021c33d022f9fad7649232405e5f357cc2da773b
anandB000 <anandrajbalu00@gmail.com>
2018-03-24 00:49:52 +0530

    Not showing category meta

---
+++ include/Core/Posts/Metabox/DeletePostsByCategoryMetabox.php
@@ -0,0 +1,121 @@
+<?php
+namespace BulkWP\BulkDelete\Core\Posts\Metabox;
+
+use BulkWP\BulkDelete\Core\Posts\PostsMetabox;
+
+defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
+
+/**
+ * Delete Posts by Category Metabox.
+ *
+ * @since 6.0.0
+ */
+class DeletePostsByCategoryMetabox extends PostsMetabox {
+	protected function initialize() {
+		$this->item_type     = 'posts';
+		$this->field_slug    = 'cats';
+		$this->meta_box_slug = 'bd_by_category';
+		$this->action        = 'delete_posts_by_category';
+		$this->cron_hook     = 'do-bulk-delete-cat';
+		$this->scheduler_url = 'http://bulkwp.com/addons/scheduler-for-deleting-posts-by-category/?utm_source=wpadmin&utm_campaign=BulkDelete&utm_medium=buynow&utm_content=bd-sc';
+		$this->messages      = array(
+			'box_label' => __( 'By Post Category', 'bulk-delete' ),
+			'scheduled' => __( 'The selected posts are scheduled for deletion', 'bulk-delete' ),
+		);
+	}
+
+	public function render() {
+        ?>
+        <!-- Category Start-->
+        <h4><?php _e( 'Select the post type from which you want to delete posts by category', 'bulk-delete' ); ?></h4>
+        <fieldset class="options">
+            <table class="optiontable">
+				<?php bd_render_post_type_dropdown( 'cats' ); ?>
+            </table>
+
+            <h4><?php _e( 'Select the categories from which you wan to delete posts', 'bulk-delete' ); ?></h4>
+            <p><?php _e( 'Note: The post count below for each category is the total number of posts in that category, irrespective of post type', 'bulk-delete' ); ?>.</p>
+			<?php
+			$bd_select2_ajax_limit_categories = apply_filters( 'bd_select2_ajax_limit_categories', Bulk_Delete::MAX_SELECT2_LIMIT );
+
+			$categories = get_categories( array(
+					'hide_empty'    => false,
+					'number'        => $bd_select2_ajax_limit_categories,
+				)
+			);
+			?>
+            <table class="form-table">
+                <tr>
+                    <td scope="row">
+						<?php if( count($categories) >= $bd_select2_ajax_limit_categories ){?>
+                            <select class="select2Ajax" name="smbd_cats[]" data-taxonomy="category" multiple data-placeholder="<?php _e( 'Select Categories', 'bulk-delete' ); ?>">
+                                <option value="all" selected="selected"><?php _e( 'All Categories', 'bulk-delete' ); ?></option>
+                            </select>
+						<?php }else{?>
+                            <select class="select2" name="smbd_cats[]" multiple data-placeholder="<?php _e( 'Select Categories', 'bulk-delete' ); ?>">
+                                <option value="all" selected="selected"><?php _e( 'All Categories', 'bulk-delete' ); ?></option>
+								<?php foreach ( $categories as $category ) { ?>
+                                    <option value="<?php echo $category->cat_ID; ?>"><?php echo $category->cat_name, ' (', $category->count, ' ', __( 'Posts', 'bulk-delete' ), ')'; ?></option>
+								<?php } ?>
+                            </select>
+						<?php }?>
+                    </td>
+                </tr>
+            </table>
+
+			<table class="optiontable">
+				<?php
+				$this->render_filtering_table_header();
+				$this->render_restrict_settings();
+				$this->render_delete_settings();
+				$this->render_private_post_settings();
+				$this->render_limit_settings();
+				$this->render_cron_settings();
+				?>
+			</table>
+
+		</fieldset>
+<?php
+		$this->render_submit_button( 'delete_posts_by_category' );
+	}
+
+	protected function convert_user_input_to_options( $request, $options ) {
+		$options['post_type']     = bd_array_get( $_POST, 'smbd_cats_post_type', 'post' );
+		$options['selected_cats'] = bd_array_get( $_POST, 'smbd_cats' );
+		$options['private']       = bd_array_get_bool( $_POST, 'smbd_cats_private', false );
+
+		return $options;
+	}
+
+	public function delete( $delete_options ) {
+		if ( array_key_exists( 'tags_op', $delete_options ) ) {
+			$delete_options['date_op'] = $delete_options['tags_op'];
+			$delete_options['days']    = $delete_options['tags_days'];
+		}
+
+		$delete_options = apply_filters( 'bd_delete_options', $delete_options );
+
+		$options       = array();
+		$selected_tags = $delete_options['selected_tags'];
+		if ( in_array( 'all', $selected_tags ) ) {
+			$options['tag__not__in'] = array(0);
+		} else {
+			$options['tag__in'] = $selected_tags;
+		}
+
+		$options  = bd_build_query_options( $delete_options, $options );
+		$post_ids = bd_query( $options );
+		foreach ( $post_ids as $post_id ) {
+			wp_delete_post( $post_id, $delete_options['force_delete'] );
+		}
+
+		$posts_deleted += count( $post_ids );
+
+		return $posts_deleted;
+	}
+
+	protected function get_success_message( $items_deleted ) {
+		/* translators: 1 Number of posts deleted */
+		return _n( 'Deleted %d post with the selected post category', 'Deleted %d posts with the selected post category', $items_deleted, 'bulk-delete' );
+	}
+}
