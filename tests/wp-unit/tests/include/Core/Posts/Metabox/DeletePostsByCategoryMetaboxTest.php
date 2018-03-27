cbfcd272c16078f2feaa32a538b106ebd98be7ab
anandB000 <anandrajbalu00@gmail.com>
2018-03-27 23:42:24 +0530

    Add tests to test deleting posts within the last x days
    Fix #218

--- tests/wp-unit/tests/include/Core/Posts/Metabox/DeletePostsByCategoryMetaboxTest.php
+++ tests/wp-unit/tests/include/Core/Posts/Metabox/DeletePostsByCategoryMetaboxTest.php
@@ -298,7 +298,7 @@ class DeletePostsByCategoryMetaboxTest extends WPCoreUnitTestCase {
 		// call our method.
 		$delete_options = array(
 			'post_type'      => array( 'post' ),
-			'selected_cats'  => array( $cat1 ),
+			'selected_cats'  => array( $cat1, $cat2 ),
 			'restrict'       => true,
 			'date_op'        => 'before',
 			'days'           => 1,
@@ -321,5 +321,71 @@ class DeletePostsByCategoryMetaboxTest extends WPCoreUnitTestCase {
 
 		$this->assertEquals( 0, count( $posts_in_cat1 ) );
 		$this->assertEquals( 1, count( $posts_in_cat2 ) );
+	}
+
+	/**
+	 * Add tests to test deleting posts that are posted within the last x days.
+	 */
+	public function test_for_delete_posts_last_x_days() {
+		//Set post publish date
+		$day_post = date('Y-m-d H:i:s', strtotime('-2 day'));
+		// Create two categories.
+		$cat1 = $this->factory->category->create( array( 'name' => 'cat1' ) );
+		$cat2 = $this->factory->category->create( array( 'name' => 'cat2' ) );
+
+		// Assign the cat1 and cat2 to post1 and post2
+		$post1 = $this->factory->post->create( array( 'post_title' => 'post1', 'post_status' => 'publish', 'post_category' => array( $cat1 ) ) );
+		$time = current_time('mysql');
+		// Update post1 publish date.
+		wp_update_post(
+			array (
+			'ID'            => $post1,
+			'post_date'     => $day_post,
+			'post_date_gmt' => $day_post,
+			)
+		);
+
+		$post2 = $this->factory->post->create( array( 'post_title' => 'post2', 'post_status' => 'publish', 'post_category' => array( $cat2 ) ) );
+
+		// Assert that each post status is publish.
+		$post1_status = get_post_status( $post1 );
+		$post2_status = get_post_status( $post2 );
+
+		$this->assertEquals( 'publish', $post1_status );
+		$this->assertEquals( 'publish', $post2_status );
+
+		// Assert that each category has one post.
+		$posts_in_cat1 = $this->get_posts_by_category( $cat1 );
+		$posts_in_cat2 = $this->get_posts_by_category( $cat2 );
+
+		$this->assertEquals( 1, count( $posts_in_cat1 ) );
+		$this->assertEquals( 1, count( $posts_in_cat2 ) );
+
+		// call our method.
+		$delete_options = array(
+			'post_type'      => array( 'post' ),
+			'selected_cats'  => array( $cat1, $cat2 ),
+			'restrict'       => true,
+			'date_op'        => 'after',
+			'days'           => 1,
+			'private'        => false,
+			'limit_to'       => false,
+			'force_delete'   => false,
+		);
+		$this->metabox->delete( $delete_options );
+
+		// Assert that post2 status moved to trash post1 status is publish.
+		$post1_status = get_post_status( $post1 );
+		$post2_status = get_post_status( $post2 );
+
+		$this->assertEquals( 'publish', $post1_status );
+		$this->assertEquals( 'trash', $post2_status );
+
+		// Assert that cat2 has no post and cat1 has one post.
+		$posts_in_cat1 = $this->get_posts_by_category( $cat1 );
+		$posts_in_cat2 = $this->get_posts_by_category( $cat2 );
+
+		$this->assertEquals( 1, count( $posts_in_cat1 ) );
+		$this->assertEquals( 0, count( $posts_in_cat2 ) );
 	}
 }
