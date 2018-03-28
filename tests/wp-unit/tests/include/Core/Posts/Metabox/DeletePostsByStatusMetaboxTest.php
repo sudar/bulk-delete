54c3edb79549ba7d53f956d085822d9796116b3b
Rajan <rajanit2000@gmail.com>
2018-03-29 02:41:49 +0530

    Add tests to test deleting posts that are older than x days #217

--- tests/wp-unit/tests/include/Core/Posts/Metabox/DeletePostsByStatusMetaboxTest.php
+++ tests/wp-unit/tests/include/Core/Posts/Metabox/DeletePostsByStatusMetaboxTest.php
@@ -109,4 +109,35 @@ class DeletePostsByStatusMetaboxTest extends WPCoreUnitTestCase {
 		$trash_post = $this->get_posts_by_status( 'trash' );
 		$this->assertEquals( 0, count( $trash_post ) );
 	}
+
+	public function test_that_are_older_than_x_days_post_can_be_deleted() {
+		$date = date("Y-m-d H:i:s", strtotime("-5 day") );
+
+		$published_post = $this->factory->post->create_many( 10, array(
+			'post_type' => 'post',
+			'post_date' => $date,
+		) );
+
+		$this->assertEquals( 10, count( $published_post ) );
+
+		$delete_options = array(
+			'publish'      => 'published_posts',
+			'drafts'       => '',
+			'pending'      => '',
+			'future'       => '',
+			'private'      => '',
+			'limit_to'     => -1,
+			'restrict'     => true,
+			'force_delete' => false,
+			'date_op'      => 'before',
+			'days'         => '3',
+		);
+
+		$posts_deleted = $this->metabox->delete( $delete_options );
+		$this->assertEquals( 10, $posts_deleted );
+
+		$published_post = $this->get_posts_by_status();
+		$this->assertEquals( 0, count( $published_post ) );
+
+	}
 }
