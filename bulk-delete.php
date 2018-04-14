79f74ea74d5c9c97a8a6171d8296210623f3a15d
Sudar Muthu <sudar@sudarmuthu.com>
2018-04-14 07:40:11 +0530

    Drop support for PHP 5.2
    Deactivate the plugin and recommend v5.6.1 if PHP 5.2 is detected.
    
    Fix #213

--- bulk-delete.php
+++ bulk-delete.php
@@ -32,7 +32,49 @@
  */
 defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
 
-// TODO: Do PHP Version check and proceed only if PHP 5.3 or above.
+// Include the stub of the old `Bulk_Delete` class, so that old add-ons don't generate a fatal error.
 require_once 'include/deprecated/old-bulk-delete.php';
+
+if ( version_compare( PHP_VERSION, '5.3.0', '<' ) ) {
+	/**
+	 * Version 6.0.0 of the Email Log plugin dropped support for PHP 5.2.
+	 * If you are still struck with PHP 5.2 and can't update, then use v5.6.1 of the plugin.
+	 * But note that some add-ons may not work.
+	 *
+	 * @see   http://sudarmuthu.com/blog/why-i-am-dropping-support-for-php-5-2-in-my-wordpress-plugins/
+	 * @since 6.0.0
+	 */
+	function bulk_delete_compatibility_notice() {
+		?>
+		<div class="error">
+			<p>
+				<?php
+				printf(
+					__( 'Bulk Delete requires at least PHP 5.3 to function properly. Please upgrade PHP or use <a href="%s">v5.6.1 of Bulk Delete</a>.', 'bulk-delete' ), // @codingStandardsIgnoreLine
+					'https://downloads.wordpress.org/plugin/bulk-delete.5.6.1.zip'
+				);
+				?>
+			</p>
+		</div>
+		<?php
+	}
+
+	add_action( 'admin_notices', 'bulk_delete_compatibility_notice' );
+
+	/**
+	 * Deactivate Bulk Delete.
+	 *
+	 * @since 6.0.0
+	 */
+	function bulk_delete_deactivate() {
+		deactivate_plugins( plugin_basename( __FILE__ ) );
+	}
+
+	add_action( 'admin_init', 'bulk_delete_deactivate' );
+
+	return;
+}
+
+// PHP is at least 5.3, so we can safely include namespace code.
 require_once 'load-bulk-delete.php';
 bulk_delete_load( __FILE__ );
