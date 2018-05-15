170f67c4ef5dac8289cffa8c788570b9c7c6cb62
Rajan <rajanit2000@gmail.com>
2018-05-15 05:47:54 +0530

    Add tests for deleting single Post meta from the custom post type Add tests for deleting more than two Post meta from the custom post type
    #271

--- tests/wp-unit/include/Core/Metas/Modules/DeletePostMetaModuleTest.php
+++ tests/wp-unit/include/Core/Metas/Modules/DeletePostMetaModuleTest.php
@@ -64,4 +64,56 @@ class DeletePostMetaModuleTest extends WPCoreUnitTestCase {
 		$post_meta = get_post_meta( $post, 'time' );
 		$this->assertEquals( 0, count( $post_meta ) );
 	}
+
+	public function test_that_single_post_meta_from_the_custom_post_type() {
+		$post = $this->factory->post->create( array( 'post_title' => 'Test Post', 'post_type' => 'custom' ) );
+
+		add_post_meta( $post, 'time', '10/10/2018' );
+
+		$post_meta = get_post_meta( $post, 'time' );
+		$this->assertEquals( 1, count( $post_meta ) );
+
+		$delete_options = array(
+			'post_type'    => 'custom', 
+			'limit_to'     => -1,
+			'restrict'     => false,
+			'force_delete' => false,
+			'use_value'    => 'use_key',
+			'meta_key'     => 'time',
+			'date_op'      => '',
+			'days'         => '',
+		);
+
+		$meta_deleted = $this->module->delete( $delete_options );
+
+		$post_meta = get_post_meta( $post, 'time' );
+		$this->assertEquals( 0, count( $post_meta ) );
+	}
+
+	public function test_that_more_than_one_post_meta_from_the_custom_post_type() {
+		$post = $this->factory->post->create( array( 'post_title' => 'Test Post', 'post_type' => 'custom' ) );
+
+		add_post_meta( $post, 'time', '10/10/2018' );
+		add_post_meta( $post, 'time', '11/11/2018' );
+		add_post_meta( $post, 'time', '12/12/2018' );
+
+		$post_meta = get_post_meta( $post, 'time' );
+		$this->assertEquals( 3, count( $post_meta ) );
+
+		$delete_options = array(
+			'post_type'    => 'custom', 
+			'limit_to'     => -1,
+			'restrict'     => false,
+			'force_delete' => false,
+			'use_value'    => 'use_key',
+			'meta_key'     => 'time',
+			'date_op'      => '',
+			'days'         => '',
+		);
+
+		$meta_deleted = $this->module->delete( $delete_options );
+
+		$post_meta = get_post_meta( $post, 'time' );
+		$this->assertEquals( 0, count( $post_meta ) );
+	}
 }
