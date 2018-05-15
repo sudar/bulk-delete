f8ddd2879643959bd24219cfa0fd2daaeedfb50b
Rajan <rajanit2000@gmail.com>
2018-05-15 07:53:17 +0530

    Add tests for deleting multiple user meta fields from 'admin' user role Add tests for deleting multiple user meta fields from 'subscriber' user role Add tests for deleting user meta from users in batches
    #272

--- tests/wp-unit/include/Core/Metas/DeletePostsByUserMetaModuleTest.php
+++ tests/wp-unit/include/Core/Metas/DeletePostsByUserMetaModuleTest.php
@@ -46,5 +46,88 @@ class DeletePostsByUserMetaModuleTest extends WPCoreUnitTestCase {
 
 	}
 
-	
+	public function test_deleting_single_user_meta_fields_from_subscriber_user_role() {
+		
+		$user = $this->factory->user->create( array( 'role' => 'subscriber' ));
+
+		add_user_meta( $user, 'time', '10/10/2018' );
+
+		$delete_options = array(
+			'user_role'    => 'subscriber', 
+			'meta_key'     => 'time',
+			'use_value'    => false,
+			'limit_to'     => -1,
+			'delete_options' => '',
+		);
+		$meta_deleted = $this->module->delete( $delete_options );
+
+		$user_meta = get_user_meta( $user, 'time' );
+		$this->assertEquals( 0, count( $user_meta ) );
+
+	}
+
+	public function test_deleting_multiple_user_meta_fields_from_admin_user_role() {
+		
+		$user = $this->factory->user->create( array( 'role' => 'administrator' ));
+
+		add_user_meta( $user, 'time', '10/10/2018' );
+		add_user_meta( $user, 'time', '11/10/2018' );
+		add_user_meta( $user, 'time', '12/10/2018' );
+
+		$delete_options = array(
+			'user_role'    => 'administrator', 
+			'meta_key'     => 'time',
+			'use_value'    => false,
+			'limit_to'     => -1,
+			'delete_options' => '',
+		);
+		$meta_deleted = $this->module->delete( $delete_options );
+
+		$user_meta = get_user_meta( $user, 'time' );
+		$this->assertEquals( 0, count( $user_meta ) );
+
+	}
+
+	public function test_deleting_multiple_user_meta_fields_from_subscriber_user_role() {
+		
+		$user = $this->factory->user->create( array( 'role' => 'subscriber' ));
+
+		add_user_meta( $user, 'time', '10/10/2018' );
+		add_user_meta( $user, 'time', '11/10/2018' );
+		add_user_meta( $user, 'time', '12/10/2018' );
+
+		$delete_options = array(
+			'user_role'    => 'subscriber', 
+			'meta_key'     => 'time',
+			'use_value'    => false,
+			'limit_to'     => -1,
+			'delete_options' => '',
+		);
+		$meta_deleted = $this->module->delete( $delete_options );
+
+		$user_meta = get_user_meta( $user, 'time' );
+		$this->assertEquals( 0, count( $user_meta ) );
+
+	}
+
+	public function test_delete_usermeta_in_batches() {
+		
+		$users = $this->factory->user->create_many( 20, array( 'role' => 'subscriber' ));
+
+		foreach($users as $user ){
+			add_user_meta( $user, 'time', '10/10/2018' );
+		}
+
+		$delete_options = array(
+			'user_role'    => 'subscriber', 
+			'meta_key'     => 'time',
+			'use_value'    => false,
+			'limit_to'     => 10,
+			'delete_options' => '',
+		);
+		$meta_deleted = $this->module->delete( $delete_options );
+
+		$this->assertEquals( 10, $meta_deleted );
+
+	}
 }
