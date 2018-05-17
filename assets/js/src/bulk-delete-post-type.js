0f86d0dd740429abfe0572e85bff3d2f9df19192
Rajan <rajanit2000@gmail.com>
2018-05-17 11:33:29 +0530

    Update to inline function
    Fix #268

--- assets/js/src/bulk-delete-post-type.js
+++ assets/js/src/bulk-delete-post-type.js
@@ -1,17 +1,15 @@
 jQuery( document ).ready( function () {
 	jQuery( '.select2-post' ).select2( {
 		width: '300px',
-		templateSelection: formatLabel
-	} );
+		templateSelection: function (state) {
+			if (!state.id) {
+				return state.text;
+			}
+			var parentLabel = state.element.parentElement.label;
+			var $state = jQuery(
+				'<span>' + parentLabel + '-' + state.text + '</span>'
+			);
+			return $state;
+		}
+	});
 });
-
-function formatLabel (state) {
-  if (!state.id) {
-    return state.text;
-  }
-  var parentLabel = state.element.parentElement.label;
-  var $state = jQuery(
-  	    '<span>' + parentLabel + '-' + state.text + '</span>'
-  );
-  return $state;
-}
