abef23784bcdb2d2afe8da78af8f4b70bcd961c2
Rajan <rajanit2000@gmail.com>
2018-05-23 08:23:20 +0530

    Fix js grunt issue
    Fix #311

--- assets/js/src/delete-posts-by-sticky-post.js
+++ assets/js/src/delete-posts-by-sticky-post.js
@@ -7,7 +7,7 @@ jQuery( document ).ready( function () {
 
 	jQuery("input[name='smbd_sticky_post_sticky_option']").change(function(){
 		var sticky_option = jQuery("input[name='smbd_sticky_post_sticky_option']:checked").val();
-		if( sticky_option == "show" ){
+		if( sticky_option === "show" ){
 			jQuery( '.sticky_force_delete' ).show();
 		}else{
 			jQuery( '.sticky_force_delete' ).hide();
