7624abeb3bcb60d5e7e01378cf2f59e31afa5cec
Rajan <rajanit2000@gmail.com>
2018-06-14 06:13:32 +0530

    Add tests to delete revisions for a single post Add tests to delete revisions for multiple posts
    Fix #252

---
+++ tests/wp-unit/include/Core/Posts/Modules/DeletePostsByRevisionModuleTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace BulkWP\BulkDelete\Core\Posts\Modules;
+
+use BulkWP\Tests\WPCore\WPCoreUnitTestCase;
+
+/**
+ * Test Deletion of Posts by revisions.
+ *
+ * Tests \BulkWP\BulkDelete\Core\Posts\Modules\DeletePostsByRevisionModule
+ *
+ * @since 6.0.0
+ */
+class DeletePostsByRevisionModuleTest extends WPCoreUnitTestCase {
+
+	/**
+	 * The module that is getting tested.
+	 *
+	 * @var \BulkWP\BulkDelete\Core\Posts\Modules\DeletePostsByRevisionModule
+	 */
+	protected $module;
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->module = new DeletePostsByRevisionModule();
+	}
+
+	/**
+	 * Test delete revisions for a single post
+	 */
+	public function test_delete_revisions_for_single_post() {
+		
+		$post = $this->factory->post->create( array(
+			'post_type' => 'post',
+		) );
+
+		$revision_post_1 = array(
+			'ID'           => $post,
+			'post_title'   => rand(),
+			'post_content' => md5( rand() ),
+		);
+
+		wp_update_post( $revision_post_1 );
+
+		$revision_post_2 = array(
+			'ID'           => $post,
+			'post_title'   => rand(),
+			'post_content' => md5( rand() ),
+		);
+
+		wp_update_post( $revision_post_2 );
+
+		$delete_options = array(
+			'revisions'    => 'revisions',
+			'limit_to'     => -1,
+			'restrict'     => false,
+			'force_delete' => false,
+		);
+
+		$posts_deleted = $this->module->delete( $delete_options );
+		$this->assertEquals( 2, $posts_deleted );
+
+	}
+
+	/**
+	 * Test delete revisions for a multiple post
+	 */
+	public function test_delete_revisions_for_multiple_post() {
+		
+		$posts = $this->factory->post->create_many( 10, array(
+			'post_type' => 'post',
+		) );
+
+		foreach( $posts as $post ){
+
+			$revision_post = array(
+				'ID'           => $post,
+				'post_title'   => rand(),
+				'post_content' => md5( rand() ),
+			);
+
+			wp_update_post( $revision_post );
+
+		}
+
+		$delete_options = array(
+			'revisions'    => 'revisions',
+			'limit_to'     => -1,
+			'restrict'     => false,
+			'force_delete' => false,
+		);
+
+		$posts_deleted = $this->module->delete( $delete_options );
+		$this->assertEquals( 10, $posts_deleted );
+
+	}
+
+}
