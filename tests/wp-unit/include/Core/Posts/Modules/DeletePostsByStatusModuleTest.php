e493fb614652420418d7138ba885fbdea07c1e27
rajanit2000 <rajanit2000@gmail.com>
2018-07-12 08:10:46 +0530

    Add missing test cases Fix #358

--- tests/wp-unit/include/Core/Posts/Modules/DeletePostsByStatusModuleTest.php
+++ tests/wp-unit/include/Core/Posts/Modules/DeletePostsByStatusModuleTest.php
@@ -63,41 +63,11 @@ class DeletePostsByStatusModuleTest extends WPCoreUnitTestCase {
 	}
 
 	/**
-	 * Test that posts from single post status can be trashed.
-	 */
-	public function test_that_draft_posts_can_be_trashed() {
-		$this->factory->post->create_many( 10, array(
-			'post_type'   => 'post',
-			'post_status' => 'draft',
-		) );
-
-		$draft_posts = $this->get_posts_by_status( 'draft' );
-		$this->assertEquals( 10, count( $draft_posts ) );
-
-		$delete_options = array(
-			'post_status'  => array( 'draft' ),
-			'limit_to'     => -1,
-			'restrict'     => false,
-			'force_delete' => false,
-		);
-
-		$posts_deleted = $this->module->delete( $delete_options );
-		$this->assertEquals( 10, $posts_deleted );
-
-		$published_posts = $this->get_posts_by_status();
-		$this->assertEquals( 0, count( $published_posts ) );
-
-		$trash_posts = $this->get_posts_by_status( 'trash' );
-		$this->assertEquals( 10, count( $trash_posts ) );
-	}
-
-	/**
-	 * Test that posts from two post status can be permanently deleted.
+	 * Test that posts from two post status can be deleted.
 	 */
 	public function test_that_published_posts_can_be_deleted() {
 		$this->factory->post->create_many( 10, array(
-			'post_type'   => 'post',
-			'post_status' => 'publish',
+			'post_type' => 'post',
 		) );
 
 		$published_posts = $this->get_posts_by_status();
@@ -126,6 +96,35 @@ class DeletePostsByStatusModuleTest extends WPCoreUnitTestCase {
 
 		$trash_posts = $this->get_posts_by_status( 'trash' );
 		$this->assertEquals( 0, count( $trash_posts ) );
+	}
+
+	/**
+	 * Test that posts from single post status can be trashed.
+	 */
+	public function test_that_draft_posts_can_be_trashed() {
+		$this->factory->post->create_many( 10, array(
+			'post_type'   => 'post',
+			'post_status' => 'draft',
+		) );
+
+		$draft_posts = $this->get_posts_by_status( 'draft' );
+		$this->assertEquals( 10, count( $draft_posts ) );
+
+		$delete_options = array(
+			'post_status'  => array( 'draft' ),
+			'limit_to'     => -1,
+			'restrict'     => false,
+			'force_delete' => false,
+		);
+
+		$posts_deleted = $this->module->delete( $delete_options );
+		$this->assertEquals( 10, $posts_deleted );
+
+		$published_posts = $this->get_posts_by_status();
+		$this->assertEquals( 0, count( $published_posts ) );
+
+		$trash_posts = $this->get_posts_by_status( 'trash' );
+		$this->assertEquals( 10, count( $trash_posts ) );
 	}
 
 	/**
@@ -160,6 +159,35 @@ class DeletePostsByStatusModuleTest extends WPCoreUnitTestCase {
 	/**
 	 * Test that private posts can be trashed.
 	 */
+	public function test_that_private_posts_can_be_trashed() {
+		$this->factory->post->create_many( 10, array(
+			'post_type'   => 'post',
+			'post_status' => 'private',
+		) );
+
+		$private_posts = $this->get_posts_by_status( 'private' );
+		$this->assertEquals( 10, count( $private_posts ) );
+
+		$delete_options = array(
+			'post_status'  => array( 'private' ),
+			'limit_to'     => -1,
+			'restrict'     => false,
+			'force_delete' => false,
+		);
+
+		$posts_deleted = $this->module->delete( $delete_options );
+		$this->assertEquals( 10, $posts_deleted );
+
+		$private_posts = $this->get_posts_by_status( 'private' );
+		$this->assertEquals( 0, count( $private_posts ) );
+
+		$trash_posts = $this->get_posts_by_status( 'trash' );
+		$this->assertEquals( 10, count( $trash_posts ) );
+	}
+
+	/**
+	 * Test that private posts can be deleted.
+	 */
 	public function test_that_private_posts_can_be_deleted() {
 		$this->factory->post->create_many( 10, array(
 			'post_type'   => 'post',
@@ -189,7 +217,7 @@ class DeletePostsByStatusModuleTest extends WPCoreUnitTestCase {
 	/**
 	 * Test date filter (older than x days) with two post status (published posts and draft posts).
 	 */
-	public function test_that_published_posts_that_are_older_than_x_days_can_be_deleted() {
+	public function test_that_published_posts_that_are_older_than_x_days_can_be_trashed() {
 		$date = date( 'Y-m-d H:i:s', strtotime( '-5 day' ) );
 
 		$published_posts = $this->factory->post->create_many( 10, array(
@@ -228,9 +256,50 @@ class DeletePostsByStatusModuleTest extends WPCoreUnitTestCase {
 	}
 
 	/**
+	 * Test date filter (older than x days) with two post status (published posts and draft posts).
+	 */
+	public function test_that_published_posts_that_are_older_than_x_days_can_be_deleted() {
+		$date = date( 'Y-m-d H:i:s', strtotime( '-5 day' ) );
+
+		$published_posts = $this->factory->post->create_many( 10, array(
+			'post_type'   => 'post',
+			'post_status' => 'publish',
+			'post_date'   => $date,
+		) );
+
+		$this->assertEquals( 10, count( $published_posts ) );
+
+		$draft_posts = $this->factory->post->create_many( 10, array(
+			'post_type'   => 'post',
+			'post_status' => 'draft',
+			'post_date'   => $date,
+		) );
+
+		$this->assertEquals( 10, count( $draft_posts ) );
+
+		$delete_options = array(
+			'post_status'  => array( 'publish', 'draft' ),
+			'limit_to'     => -1,
+			'restrict'     => true,
+			'force_delete' => true,
+			'date_op'      => 'before',
+			'days'         => '3',
+		);
+
+		$posts_deleted = $this->module->delete( $delete_options );
+		$this->assertEquals( 20, $posts_deleted );
+
+		$published_posts = $this->get_posts_by_status();
+		$this->assertEquals( 0, count( $published_posts ) );
+
+		$trash_posts = $this->get_posts_by_status( 'trash' );
+		$this->assertEquals( 0, count( $trash_posts ) );
+	}
+
+	/**
 	 * Test date filter (older than x days) with single post status (draft posts).
 	 */
-	public function test_that_draft_posts_that_are_older_than_x_days_can_be_deleted() {
+	public function test_that_draft_posts_that_are_older_than_x_days_can_be_trashed() {
 		$date = date( 'Y-m-d H:i:s', strtotime( '-5 day' ) );
 
 		$draft_posts = $this->factory->post->create_many( 10, array(
@@ -258,6 +327,80 @@ class DeletePostsByStatusModuleTest extends WPCoreUnitTestCase {
 
 		$trash_posts = $this->get_posts_by_status( 'trash' );
 		$this->assertEquals( 10, count( $trash_posts ) );
+	}
+
+	/**
+	 * Test date filter (older than x days) with single post status (draft posts).
+	 */
+	public function test_that_draft_posts_that_are_older_than_x_days_can_be_deleted() {
+		$date = date( 'Y-m-d H:i:s', strtotime( '-5 day' ) );
+
+		$draft_posts = $this->factory->post->create_many( 10, array(
+			'post_type'   => 'post',
+			'post_status' => 'draft',
+			'post_date'   => $date,
+		) );
+
+		$this->assertEquals( 10, count( $draft_posts ) );
+
+		$delete_options = array(
+			'post_status'  => array( 'draft' ),
+			'limit_to'     => -1,
+			'restrict'     => true,
+			'force_delete' => true,
+			'date_op'      => 'before',
+			'days'         => '3',
+		);
+
+		$posts_deleted = $this->module->delete( $delete_options );
+		$this->assertEquals( 10, $posts_deleted );
+
+		$published_posts = $this->get_posts_by_status();
+		$this->assertEquals( 0, count( $published_posts ) );
+
+		$trash_posts = $this->get_posts_by_status( 'trash' );
+		$this->assertEquals( 0, count( $trash_posts ) );
+	}
+
+	/**
+	 * Test date filter (within the last x days) with two post status.
+	 */
+	public function test_that_published_posts_that_are_posted_within_the_last_x_days_can_be_trashed() {
+		$date = date( 'Y-m-d H:i:s', strtotime( '-3 day' ) );
+
+		$published_posts = $this->factory->post->create_many( 10, array(
+			'post_type'   => 'post',
+			'post_status' => 'publish',
+			'post_date'   => $date,
+		) );
+
+		$this->assertEquals( 10, count( $published_posts ) );
+
+		$draft_posts = $this->factory->post->create_many( 10, array(
+			'post_type'   => 'post',
+			'post_status' => 'draft',
+			'post_date'   => $date,
+		) );
+
+		$this->assertEquals( 10, count( $draft_posts ) );
+
+		$delete_options = array(
+			'post_status'  => array( 'publish', 'draft' ),
+			'limit_to'     => -1,
+			'restrict'     => true,
+			'force_delete' => false,
+			'date_op'      => 'after',
+			'days'         => '5',
+		);
+
+		$posts_deleted = $this->module->delete( $delete_options );
+		$this->assertEquals( 20, $posts_deleted );
+
+		$published_posts = $this->get_posts_by_status();
+		$this->assertEquals( 0, count( $published_posts ) );
+
+		$trash_posts = $this->get_posts_by_status( 'trash' );
+		$this->assertEquals( 20, count( $trash_posts ) );
 	}
 
 	/**
@@ -286,7 +429,7 @@ class DeletePostsByStatusModuleTest extends WPCoreUnitTestCase {
 			'post_status'  => array( 'publish', 'draft' ),
 			'limit_to'     => -1,
 			'restrict'     => true,
-			'force_delete' => false,
+			'force_delete' => true,
 			'date_op'      => 'after',
 			'days'         => '5',
 		);
@@ -298,13 +441,13 @@ class DeletePostsByStatusModuleTest extends WPCoreUnitTestCase {
 		$this->assertEquals( 0, count( $published_posts ) );
 
 		$trash_posts = $this->get_posts_by_status( 'trash' );
-		$this->assertEquals( 20, count( $trash_posts ) );
+		$this->assertEquals( 0, count( $trash_posts ) );
 	}
 
 	/**
 	 * Test date filter (within the last x days) with single post status.
 	 */
-	public function test_that_draft_posts_that_are_posted_within_the_last_x_days_can_be_deleted() {
+	public function test_that_draft_posts_that_are_posted_within_the_last_x_days_can_be_trashed() {
 		$date = date( 'Y-m-d H:i:s', strtotime( '-3 day' ) );
 
 		$draft_posts = $this->factory->post->create_many( 10, array(
@@ -332,6 +475,39 @@ class DeletePostsByStatusModuleTest extends WPCoreUnitTestCase {
 
 		$trash_posts = $this->get_posts_by_status( 'trash' );
 		$this->assertEquals( 10, count( $trash_posts ) );
+	}
+
+	/**
+	 * Test date filter (within the last x days) with single post status.
+	 */
+	public function test_that_draft_posts_that_are_posted_within_the_last_x_days_can_be_deleted() {
+		$date = date( 'Y-m-d H:i:s', strtotime( '-3 day' ) );
+
+		$draft_posts = $this->factory->post->create_many( 10, array(
+			'post_type'   => 'post',
+			'post_status' => 'draft',
+			'post_date'   => $date,
+		) );
+
+		$this->assertEquals( 10, count( $draft_posts ) );
+
+		$delete_options = array(
+			'post_status'  => array( 'draft' ),
+			'limit_to'     => -1,
+			'restrict'     => true,
+			'force_delete' => true,
+			'date_op'      => 'after',
+			'days'         => '5',
+		);
+
+		$posts_deleted = $this->module->delete( $delete_options );
+		$this->assertEquals( 10, $posts_deleted );
+
+		$published_posts = $this->get_posts_by_status();
+		$this->assertEquals( 0, count( $published_posts ) );
+
+		$trash_posts = $this->get_posts_by_status( 'trash' );
+		$this->assertEquals( 0, count( $trash_posts ) );
 	}
 
 
