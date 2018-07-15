da507b50d02ae5906b9aa501b93cdbdca216d673
Rajan <rajanit2000@gmail.com>
2018-07-15 19:33:18 +0530

    Remove unused params Update functions based on trash / delete
    Fix #353

--- tests/wp-unit/include/Core/Posts/Modules/DeletePostsByRevisionModuleTest.php
+++ tests/wp-unit/include/Core/Posts/Modules/DeletePostsByRevisionModuleTest.php
@@ -27,6 +27,39 @@ class DeletePostsByRevisionModuleTest extends WPCoreUnitTestCase {
 	}
 
 	/**
+	 * Test trash revisions for a single post
+	 */
+	public function test_trash_revisions_for_single_post() {
+		
+		$post_id = $this->factory->post->create( array(
+			'post_type' => 'post',
+		) );
+
+		$revision_post_1 = array(
+			'ID'           => $post_id,
+			'post_title'   => rand(),
+			'post_content' => md5( rand() ),
+		);
+
+		wp_update_post( $revision_post_1 );
+
+		$revision_post_2 = array(
+			'ID'           => $post_id,
+			'post_title'   => rand(),
+			'post_content' => md5( rand() ),
+		);
+
+		wp_update_post( $revision_post_2 );
+
+		$delete_options = array(
+			'revisions'    => 'revisions',
+		);
+
+		$posts_deleted = $this->module->delete( $delete_options );
+		$this->assertEquals( 2, $posts_deleted );
+	}
+
+	/**
 	 * Test delete revisions for a single post
 	 */
 	public function test_delete_revisions_for_single_post() {
@@ -53,13 +86,43 @@ class DeletePostsByRevisionModuleTest extends WPCoreUnitTestCase {
 
 		$delete_options = array(
 			'revisions'    => 'revisions',
-			'limit_to'     => -1,
-			'restrict'     => false,
-			'force_delete' => false,
+			'force_delete' => true,
 		);
 
 		$posts_deleted = $this->module->delete( $delete_options );
 		$this->assertEquals( 2, $posts_deleted );
+
+		$trash_posts = $this->get_posts_by_status( 'trash' );
+		$this->assertEquals( 0, count( $trash_posts ) );
+	}
+
+	/**
+	 * Test trash revisions for a multiple post
+	 */
+	public function test_trash_revisions_for_multiple_post() {
+		
+		$post_ids = $this->factory->post->create_many( 10, array(
+			'post_type' => 'post',
+		) );
+
+		foreach( $post_ids as $post_id ){
+
+			$revision_post = array(
+				'ID'           => $post_id,
+				'post_title'   => rand(),
+				'post_content' => md5( rand() ),
+			);
+
+			wp_update_post( $revision_post );
+
+		}
+
+		$delete_options = array(
+			'revisions'    => 'revisions',
+		);
+
+		$posts_deleted = $this->module->delete( $delete_options );
+		$this->assertEquals( 10, $posts_deleted );
 
 	}
 
@@ -86,13 +149,14 @@ class DeletePostsByRevisionModuleTest extends WPCoreUnitTestCase {
 
 		$delete_options = array(
 			'revisions'    => 'revisions',
-			'limit_to'     => -1,
-			'restrict'     => false,
-			'force_delete' => false,
+			'force_delete' => true,
 		);
 
 		$posts_deleted = $this->module->delete( $delete_options );
 		$this->assertEquals( 10, $posts_deleted );
+
+		$trash_posts = $this->get_posts_by_status( 'trash' );
+		$this->assertEquals( 0, count( $trash_posts ) );
 
 	}
 
