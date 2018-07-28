4154a18f097fb4f7e8532a8cb46e19098e04bc2c
Rajan <rajanit2000@gmail.com>
2018-07-28 14:54:25 +0530

    Fix PHPCS issues
    Fix #249

--- tests/wp-unit/include/Core/Posts/Modules/DeletePostsByTaxonomyModuleTest.php
+++ tests/wp-unit/include/Core/Posts/Modules/DeletePostsByTaxonomyModuleTest.php
@@ -20,6 +20,9 @@ class DeletePostsByTaxonomyModuleTest extends WPCoreUnitTestCase {
 	 */
 	protected $module;
 
+	/**
+	 * Setup the DeletePostsByTaxonomyModule Module.
+	 */
 	public function setUp() {
 		parent::setUp();
 
@@ -34,18 +37,24 @@ class DeletePostsByTaxonomyModuleTest extends WPCoreUnitTestCase {
 		$cat1 = $this->factory->category->create( array( 'name' => 'cat1' ) );
 
 		// Assign the cat1 to post1.
-		$post1 = $this->factory->post->create( array( 'post_title' => 'post1', 'post_status' => 'publish', 'post_category' => array( $cat1 ) ) );
-		
+		$post1 = $this->factory->post->create(
+			array(
+				'post_title'    => 'post1',
+				'post_status'   => 'publish',
+				'post_category' => array( $cat1 ),
+			)
+		);
+
 		$posts_in_cat1 = $this->get_posts_by_category( $cat1 );
-		
+
 		$this->assertEquals( 1, count( $posts_in_cat1 ) );
-		
+
 		// call our method.
 		$delete_options = array(
 			'selected_taxs'      => 'category',
 			'selected_tax_terms' => array( 'cat1' ),
 		);
-		$posts_deleted = $this->module->delete( $delete_options );
+		$posts_deleted  = $this->module->delete( $delete_options );
 
 		// Assert that delete method has deleted post.
 		$this->assertEquals( 1, $posts_deleted );
@@ -66,25 +75,32 @@ class DeletePostsByTaxonomyModuleTest extends WPCoreUnitTestCase {
 	 */
 	public function test_that_trash_posts_from_built_in_taxonomy_terms_in_a_custom_post_type() {
 		// Create category.
-		$cat1 = $this->factory->category->create( array( 'name' => 'cat1' ) );
+		$cat1      = $this->factory->category->create( array( 'name' => 'cat1' ) );
 		$post_type = 'custom';
 
 		register_post_type( $post_type );
 		register_taxonomy( 'category', array( $post_type ) );
 		// Assign the cat1 to post1.
-		$this->factory->post->create( array( 'post_title' => 'post1', 'post_type' => $post_type, 'post_status' => 'publish', 'post_category' => array( $cat1 ) ) );
-		
+		$this->factory->post->create(
+			array(
+				'post_title'    => 'post1',
+				'post_type'     => $post_type,
+				'post_status'   => 'publish',
+				'post_category' => array( $cat1 ),
+			)
+		);
+
 		$posts_in_cat1 = $this->get_posts_by_category( $cat1, $post_type );
-		
+
 		$this->assertEquals( 1, count( $posts_in_cat1 ) );
-		
+
 		// call our method.
 		$delete_options = array(
 			'post_type'          => $post_type,
 			'selected_taxs'      => 'category',
 			'selected_tax_terms' => array( 'cat1' ),
 		);
-		$posts_deleted = $this->module->delete( $delete_options );
+		$posts_deleted  = $this->module->delete( $delete_options );
 
 		// Assert that delete method has deleted post.
 		$this->assertEquals( 1, $posts_deleted );
@@ -103,13 +119,13 @@ class DeletePostsByTaxonomyModuleTest extends WPCoreUnitTestCase {
 	 * Test trash posts from a single taxonomy term
 	 */
 	public function test_that_trash_posts_from_single_taxonomy_term() {
-		
+
 		$taxonomy_name = 'custom';
 		$term_value    = 'Custom Term';
 		$post_type     = 'post';
-		register_taxonomy( $taxonomy_name , 'post' );
+		register_taxonomy( $taxonomy_name, 'post' );
 		$term_opt = wp_insert_term( $term_value, $taxonomy_name );
-		$count = 10;
+		$count    = 10;
 
 		$post_data = array(
 			'post_type'   => $post_type,
@@ -117,18 +133,18 @@ class DeletePostsByTaxonomyModuleTest extends WPCoreUnitTestCase {
 			'post_status' => 'publish',
 		);
 
-		for( $i = 1; $i <= $count; $i++ ){
-			$post_id  = wp_insert_post( $post_data );
+		for ( $i = 1; $i <= $count; $i++ ) {
+			$post_id = wp_insert_post( $post_data );
 			wp_set_object_terms( $post_id, array( $term_opt['term_id'] ), $taxonomy_name );
 		}
-		
+
 		// call our method.
 		$delete_options = array(
 			'post_type'          => $post_type,
 			'selected_taxs'      => $taxonomy_name,
 			'selected_tax_terms' => array( $term_value ),
 		);
-		$posts_deleted = $this->module->delete( $delete_options );
+		$posts_deleted  = $this->module->delete( $delete_options );
 
 		// Assert that delete method has deleted post.
 		$this->assertEquals( 10, $posts_deleted );
@@ -146,33 +162,33 @@ class DeletePostsByTaxonomyModuleTest extends WPCoreUnitTestCase {
 	 * Test deleting posts from a single taxonomy term
 	 */
 	public function test_that_delete_posts_from_single_taxonomy_term() {
-		
+
 		$taxonomy_name = 'custom';
 		$term_value    = 'Custom Term';
 		$post_type     = 'post';
-		register_taxonomy( $taxonomy_name , $post_type );
+		register_taxonomy( $taxonomy_name, $post_type );
 		$term_opt = wp_insert_term( $term_value, $taxonomy_name );
-		$count = 10;
+		$count    = 10;
 
 		$post_data = array(
-			'post_type'     => $post_type,
-			'post_title'    => 'Sample Post',
-			'post_status'   => 'publish',
+			'post_type'   => $post_type,
+			'post_title'  => 'Sample Post',
+			'post_status' => 'publish',
 		);
 
-		for( $i = 1; $i <= $count; $i++ ){
-			$post_id  = wp_insert_post( $post_data );
+		for ( $i = 1; $i <= $count; $i++ ) {
+			$post_id = wp_insert_post( $post_data );
 			wp_set_object_terms( $post_id, array( $term_opt['term_id'] ), $taxonomy_name );
 		}
-		
+
 		// call our method.
 		$delete_options = array(
 			'post_type'          => $post_type,
-			'selected_taxs' 	 => $taxonomy_name,
+			'selected_taxs'      => $taxonomy_name,
 			'selected_tax_terms' => array( $term_value ),
-			'force_delete'  => true,
+			'force_delete'       => true,
 		);
-		$posts_deleted = $this->module->delete( $delete_options );
+		$posts_deleted  = $this->module->delete( $delete_options );
 
 		// Assert that delete method has deleted post.
 		$this->assertEquals( 10, $posts_deleted );
@@ -190,13 +206,13 @@ class DeletePostsByTaxonomyModuleTest extends WPCoreUnitTestCase {
 	 * Test trash posts from a multiple taxonomy term
 	 */
 	public function test_that_trash_posts_from_multiple_taxonomy_term() {
-		
+
 		$taxonomy_name = 'custom';
 		$post_type     = 'post';
-		register_taxonomy( $taxonomy_name , $post_type );
+		register_taxonomy( $taxonomy_name, $post_type );
 		$term_opt_1 = wp_insert_term( 'Custom Term 1', $taxonomy_name );
 		$term_opt_2 = wp_insert_term( 'Custom Term 2', $taxonomy_name );
-		$count = 20;
+		$count      = 20;
 
 		$post_data = array(
 			'post_type'   => $post_type,
@@ -205,21 +221,21 @@ class DeletePostsByTaxonomyModuleTest extends WPCoreUnitTestCase {
 		);
 
 		$term_id = $term_opt_1['term_id'];
-		for( $i = 1; $i <= $count; $i++ ){
-			if( $i >= 10 ){
+		for ( $i = 1; $i <= $count; $i++ ) {
+			if ( $i >= 10 ) {
 				$term_id = $term_opt_2['term_id'];
 			}
-			$post_id  = wp_insert_post( $post_data );
+			$post_id = wp_insert_post( $post_data );
 			wp_set_object_terms( $post_id, array( $term_id ), $taxonomy_name );
 		}
-		
+
 		// call our method.
 		$delete_options = array(
 			'post_type'          => $post_type,
 			'selected_taxs'      => $taxonomy_name,
 			'selected_tax_terms' => array( 'Custom Term 1', 'Custom Term 2' ),
 		);
-		$posts_deleted = $this->module->delete( $delete_options );
+		$posts_deleted  = $this->module->delete( $delete_options );
 
 		// Assert that delete method has deleted post.
 		$this->assertEquals( 20, $posts_deleted );
@@ -240,13 +256,13 @@ class DeletePostsByTaxonomyModuleTest extends WPCoreUnitTestCase {
 	 * Test deleting posts from a multiple taxonomy term
 	 */
 	public function test_that_delete_posts_from_multiple_taxonomy_term() {
-		
+
 		$taxonomy_name = 'custom';
 		$post_type     = 'post';
-		register_taxonomy( $taxonomy_name , $post_type );
+		register_taxonomy( $taxonomy_name, $post_type );
 		$term_opt_1 = wp_insert_term( 'Custom Term 1', $taxonomy_name );
 		$term_opt_2 = wp_insert_term( 'Custom Term 2', $taxonomy_name );
-		$count = 20;
+		$count      = 20;
 
 		$post_data = array(
 			'post_type'   => $post_type,
@@ -255,22 +271,22 @@ class DeletePostsByTaxonomyModuleTest extends WPCoreUnitTestCase {
 		);
 
 		$term_id = $term_opt_1['term_id'];
-		for( $i = 1; $i <= $count; $i++ ){
-			if( $i >= 10 ){
+		for ( $i = 1; $i <= $count; $i++ ) {
+			if ( $i >= 10 ) {
 				$term_id = $term_opt_2['term_id'];
 			}
-			$post_id  = wp_insert_post( $post_data );
+			$post_id = wp_insert_post( $post_data );
 			wp_set_object_terms( $post_id, array( $term_id ), $taxonomy_name );
 		}
-		
+
 		// call our method.
 		$delete_options = array(
 			'post_type'          => $post_type,
 			'selected_taxs'      => $taxonomy_name,
 			'selected_tax_terms' => array( 'Custom Term 1', 'Custom Term 2' ),
-			'force_delete'  	 => true,
+			'force_delete'       => true,
 		);
-		$posts_deleted = $this->module->delete( $delete_options );
+		$posts_deleted  = $this->module->delete( $delete_options );
 
 		// Assert that delete method has deleted post.
 		$this->assertEquals( 20, $posts_deleted );
@@ -291,13 +307,13 @@ class DeletePostsByTaxonomyModuleTest extends WPCoreUnitTestCase {
 	 * Test trash posts from a single taxonomy term custom post type
 	 */
 	public function test_that_trash_custom_posts_from_single_taxonomy_term() {
-		
+
 		$taxonomy_name = 'custom';
 		$term_value    = 'Custom Term';
 		$post_type     = 'book';
-		register_taxonomy( $taxonomy_name , $post_type );
+		register_taxonomy( $taxonomy_name, $post_type );
 		$term_opt = wp_insert_term( $term_value, $taxonomy_name );
-		$count = 10;
+		$count    = 10;
 
 		$post_data = array(
 			'post_type'   => $post_type,
@@ -305,18 +321,18 @@ class DeletePostsByTaxonomyModuleTest extends WPCoreUnitTestCase {
 			'post_status' => 'publish',
 		);
 
-		for( $i = 1; $i <= $count; $i++ ){
-			$post_id  = wp_insert_post( $post_data );
+		for ( $i = 1; $i <= $count; $i++ ) {
+			$post_id = wp_insert_post( $post_data );
 			wp_set_object_terms( $post_id, array( $term_opt['term_id'] ), $taxonomy_name );
 		}
-		
+
 		// call our method.
 		$delete_options = array(
 			'post_type'          => $post_type,
 			'selected_taxs'      => $taxonomy_name,
 			'selected_tax_terms' => array( $term_value ),
 		);
-		$posts_deleted = $this->module->delete( $delete_options );
+		$posts_deleted  = $this->module->delete( $delete_options );
 
 		// Assert that delete method has deleted post.
 		$this->assertEquals( 10, $posts_deleted );
@@ -334,13 +350,13 @@ class DeletePostsByTaxonomyModuleTest extends WPCoreUnitTestCase {
 	 * Test deleting posts from a single taxonomy term custom post type
 	 */
 	public function test_that_delete_custom_posts_from_single_taxonomy_term() {
-		
+
 		$taxonomy_name = 'custom';
 		$term_value    = 'Custom Term';
 		$post_type     = 'book';
-		register_taxonomy( $taxonomy_name , $post_type );
+		register_taxonomy( $taxonomy_name, $post_type );
 		$term_opt = wp_insert_term( $term_value, $taxonomy_name );
-		$count = 10;
+		$count    = 10;
 
 		$post_data = array(
 			'post_type'   => $post_type,
@@ -348,19 +364,19 @@ class DeletePostsByTaxonomyModuleTest extends WPCoreUnitTestCase {
 			'post_status' => 'publish',
 		);
 
-		for( $i = 1; $i <= $count; $i++ ){
-			$post_id  = wp_insert_post( $post_data );
+		for ( $i = 1; $i <= $count; $i++ ) {
+			$post_id = wp_insert_post( $post_data );
 			wp_set_object_terms( $post_id, array( $term_opt['term_id'] ), $taxonomy_name );
 		}
-		
+
 		// call our method.
 		$delete_options = array(
 			'post_type'          => $post_type,
 			'selected_taxs'      => $taxonomy_name,
 			'selected_tax_terms' => array( $term_value ),
-			'force_delete'  	 => true,
+			'force_delete'       => true,
 		);
-		$posts_deleted = $this->module->delete( $delete_options );
+		$posts_deleted  = $this->module->delete( $delete_options );
 
 		// Assert that delete method has deleted post.
 		$this->assertEquals( 10, $posts_deleted );
@@ -378,14 +394,14 @@ class DeletePostsByTaxonomyModuleTest extends WPCoreUnitTestCase {
 	 * Test trash posts thar are older than x days
 	 */
 	public function test_that_trash_posts_that_are_older_than_x_days() {
-		
+
 		$taxonomy_name = 'custom';
 		$term_value    = 'Custom Term';
 		$post_type     = 'post';
-		register_taxonomy( $taxonomy_name , $post_type );
+		register_taxonomy( $taxonomy_name, $post_type );
 		$term_opt = wp_insert_term( $term_value, $taxonomy_name );
-		$count = 10;
-		$date = date( 'Y-m-d H:i:s', strtotime( '-5 day' ) );
+		$count    = 10;
+		$date     = date( 'Y-m-d H:i:s', strtotime( '-5 day' ) );
 
 		$post_data = array(
 			'post_type'   => $post_type,
@@ -394,11 +410,11 @@ class DeletePostsByTaxonomyModuleTest extends WPCoreUnitTestCase {
 			'post_date'   => $date,
 		);
 
-		for( $i = 1; $i <= $count; $i++ ){
-			$post_id  = wp_insert_post( $post_data );
+		for ( $i = 1; $i <= $count; $i++ ) {
+			$post_id = wp_insert_post( $post_data );
 			wp_set_object_terms( $post_id, array( $term_opt['term_id'] ), $taxonomy_name );
 		}
-		
+
 		// call our method.
 		$delete_options = array(
 			'post_type'          => $post_type,
@@ -408,7 +424,7 @@ class DeletePostsByTaxonomyModuleTest extends WPCoreUnitTestCase {
 			'date_op'            => 'before',
 			'days'               => '3',
 		);
-		$posts_deleted = $this->module->delete( $delete_options );
+		$posts_deleted  = $this->module->delete( $delete_options );
 
 		// Assert that delete method has deleted post.
 		$this->assertEquals( 10, $posts_deleted );
@@ -426,14 +442,14 @@ class DeletePostsByTaxonomyModuleTest extends WPCoreUnitTestCase {
 	 * Test deleting posts that are older than x days
 	 */
 	public function test_that_delete_posts_that_are_older_than_x_days() {
-		
+
 		$taxonomy_name = 'custom';
 		$term_value    = 'Custom Term';
 		$post_type     = 'post';
-		register_taxonomy( $taxonomy_name , $post_type );
+		register_taxonomy( $taxonomy_name, $post_type );
 		$term_opt = wp_insert_term( $term_value, $taxonomy_name );
-		$count = 10;
-		$date = date( 'Y-m-d H:i:s', strtotime( '-5 day' ) );
+		$count    = 10;
+		$date     = date( 'Y-m-d H:i:s', strtotime( '-5 day' ) );
 
 		$post_data = array(
 			'post_type'   => $post_type,
@@ -442,11 +458,11 @@ class DeletePostsByTaxonomyModuleTest extends WPCoreUnitTestCase {
 			'post_date'   => $date,
 		);
 
-		for( $i = 1; $i <= $count; $i++ ){
-			$post_id  = wp_insert_post( $post_data );
+		for ( $i = 1; $i <= $count; $i++ ) {
+			$post_id = wp_insert_post( $post_data );
 			wp_set_object_terms( $post_id, array( $term_opt['term_id'] ), $taxonomy_name );
 		}
-		
+
 		// call our method.
 		$delete_options = array(
 			'post_type'          => $post_type,
@@ -457,7 +473,7 @@ class DeletePostsByTaxonomyModuleTest extends WPCoreUnitTestCase {
 			'date_op'            => 'before',
 			'days'               => '3',
 		);
-		$posts_deleted = $this->module->delete( $delete_options );
+		$posts_deleted  = $this->module->delete( $delete_options );
 
 		// Assert that delete method has deleted post.
 		$this->assertEquals( 10, $posts_deleted );
@@ -475,14 +491,14 @@ class DeletePostsByTaxonomyModuleTest extends WPCoreUnitTestCase {
 	 * Test trash posts that posted with in last x days
 	 */
 	public function test_that_trash_posts_posted_within_the_last_x_days() {
-		
+
 		$taxonomy_name = 'custom';
 		$term_value    = 'Custom Term';
 		$post_type     = 'post';
-		register_taxonomy( $taxonomy_name , $post_type );
+		register_taxonomy( $taxonomy_name, $post_type );
 		$term_opt = wp_insert_term( $term_value, $taxonomy_name );
-		$count = 10;
-		$date = date( 'Y-m-d H:i:s', strtotime( '-3 day' ) );
+		$count    = 10;
+		$date     = date( 'Y-m-d H:i:s', strtotime( '-3 day' ) );
 
 		$post_data = array(
 			'post_type'   => $post_type,
@@ -491,11 +507,11 @@ class DeletePostsByTaxonomyModuleTest extends WPCoreUnitTestCase {
 			'post_date'   => $date,
 		);
 
-		for( $i = 1; $i <= $count; $i++ ){
-			$post_id  = wp_insert_post( $post_data );
+		for ( $i = 1; $i <= $count; $i++ ) {
+			$post_id = wp_insert_post( $post_data );
 			wp_set_object_terms( $post_id, array( $term_opt['term_id'] ), $taxonomy_name );
 		}
-		
+
 		// call our method.
 		$delete_options = array(
 			'post_type'          => $post_type,
@@ -506,7 +522,7 @@ class DeletePostsByTaxonomyModuleTest extends WPCoreUnitTestCase {
 			'date_op'            => 'after',
 			'days'               => '5',
 		);
-		$posts_deleted = $this->module->delete( $delete_options );
+		$posts_deleted  = $this->module->delete( $delete_options );
 
 		// Assert that delete method has deleted post.
 		$this->assertEquals( 10, $posts_deleted );
@@ -524,14 +540,14 @@ class DeletePostsByTaxonomyModuleTest extends WPCoreUnitTestCase {
 	 * Test deleting posts that posted with in last x days
 	 */
 	public function test_that_delete_posts_posted_within_the_last_x_days() {
-		
+
 		$taxonomy_name = 'custom';
 		$term_value    = 'Custom Term';
 		$post_type     = 'post';
-		register_taxonomy( $taxonomy_name , $post_type );
+		register_taxonomy( $taxonomy_name, $post_type );
 		$term_opt = wp_insert_term( $term_value, $taxonomy_name );
-		$count = 10;
-		$date = date( 'Y-m-d H:i:s', strtotime( '-3 day' ) );
+		$count    = 10;
+		$date     = date( 'Y-m-d H:i:s', strtotime( '-3 day' ) );
 
 		$post_data = array(
 			'post_type'   => $post_type,
@@ -540,11 +556,11 @@ class DeletePostsByTaxonomyModuleTest extends WPCoreUnitTestCase {
 			'post_date'   => $date,
 		);
 
-		for( $i = 1; $i <= $count; $i++ ){
-			$post_id  = wp_insert_post( $post_data );
+		for ( $i = 1; $i <= $count; $i++ ) {
+			$post_id = wp_insert_post( $post_data );
 			wp_set_object_terms( $post_id, array( $term_opt['term_id'] ), $taxonomy_name );
 		}
-		
+
 		// call our method.
 		$delete_options = array(
 			'post_type'          => $post_type,
@@ -556,7 +572,7 @@ class DeletePostsByTaxonomyModuleTest extends WPCoreUnitTestCase {
 			'date_op'            => 'after',
 			'days'               => '5',
 		);
-		$posts_deleted = $this->module->delete( $delete_options );
+		$posts_deleted  = $this->module->delete( $delete_options );
 
 		// Assert that delete method has deleted post.
 		$this->assertEquals( 10, $posts_deleted );
@@ -574,13 +590,13 @@ class DeletePostsByTaxonomyModuleTest extends WPCoreUnitTestCase {
 	 * Test trash posts term in batches
 	 */
 	public function test_that_trash_posts_them_in_batches() {
-		
+
 		$taxonomy_name = 'custom';
 		$term_value    = 'Custom Term';
 		$post_type     = 'post';
-		register_taxonomy( $taxonomy_name , $post_type );
+		register_taxonomy( $taxonomy_name, $post_type );
 		$term_opt = wp_insert_term( $term_value, $taxonomy_name );
-		$count = 100;
+		$count    = 100;
 
 		$post_data = array(
 			'post_type'   => $post_type,
@@ -588,11 +604,11 @@ class DeletePostsByTaxonomyModuleTest extends WPCoreUnitTestCase {
 			'post_status' => 'publish',
 		);
 
-		for( $i = 1; $i <= $count; $i++ ){
-			$post_id  = wp_insert_post( $post_data );
+		for ( $i = 1; $i <= $count; $i++ ) {
+			$post_id = wp_insert_post( $post_data );
 			wp_set_object_terms( $post_id, array( $term_opt['term_id'] ), $taxonomy_name );
 		}
-		
+
 		// call our method.
 		$delete_options = array(
 			'post_type'          => $post_type,
@@ -600,7 +616,7 @@ class DeletePostsByTaxonomyModuleTest extends WPCoreUnitTestCase {
 			'selected_tax_terms' => array( $term_value ),
 			'limit_to'           => 50,
 		);
-		$posts_deleted = $this->module->delete( $delete_options );
+		$posts_deleted  = $this->module->delete( $delete_options );
 
 		// Assert that delete method has deleted post.
 		$this->assertEquals( 50, $posts_deleted );
