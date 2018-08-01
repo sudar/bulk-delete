fe946da4913e57818b6137b10b07e2c784a8fdaf
Rajan <rajanit2000@gmail.com>
2018-08-01 07:51:19 +0530

    Add batches Fix #331

--- tests/wp-unit/include/Core/Metas/Modules/DeleteCommentMetaModuleTest.php
+++ tests/wp-unit/include/Core/Metas/Modules/DeleteCommentMetaModuleTest.php
@@ -383,4 +383,44 @@ class DeleteCommentMetaModuleTest extends WPCoreUnitTestCase {
 		$this->assertEquals( 0, count( $comment_meta ) );
 	}
 
+	/**
+	 * Add to test deleting comment meta older than x days.
+	 */
+	public function test_that_deleting_comment_meta_in_batches() {
+
+		$post_type  = 'post';
+		$meta_key   = 'test_key';
+		$meta_value = 'Test Value';
+
+		// Create a post.
+		$post = $this->factory->post->create( array( 'post_title' => 'Test Post' ) );
+
+		$comment_data = array(
+			'comment_post_ID' => $post,
+			'comment_content' => 'Test Comment',
+		);
+
+		// Create 100 comments.
+		for ( $i = 1; $i <= 100; $i++ ) {
+			$comment_id = $this->factory->comment->create( $comment_data );
+
+			add_comment_meta( $comment_id, $meta_key, $meta_value );
+		}
+
+		// call our method.
+		$delete_options = array(
+			'post_type' => $post_type,
+			'use_value' => false,
+			'meta_key'  => $meta_key,
+			'limit_to'  => 50,
+			'date_op'   => '',
+			'days'      => '',
+			'restrict'  => false,
+		);
+
+		$meta_deleted = $this->module->delete( $delete_options );
+		$this->assertEquals( 50, $meta_deleted );
+
+	}
+
 }
