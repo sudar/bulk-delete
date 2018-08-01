7cc240f3631f31789cd5ffbc8383bfea66827861
Rajan <rajanit2000@gmail.com>
2018-08-01 06:32:31 +0530

    Add dataProvider Fix #331

--- tests/wp-unit/include/Core/Metas/Modules/DeleteCommentMetaModuleTest.php
+++ tests/wp-unit/include/Core/Metas/Modules/DeleteCommentMetaModuleTest.php
@@ -29,136 +29,136 @@ class DeleteCommentMetaModuleTest extends WPCoreUnitTestCase {
 		$this->module = new DeleteCommentMetaModule();
 	}
 
-	/**
-	 * Add to test deleting comment meta from one comment.
-	 */
-	public function test_that_deleting_comment_meta_from_one_comment() {
+	// /**
+	//  * Add to test deleting comment meta from one comment.
+	//  */
+	// public function test_that_deleting_comment_meta_from_one_comment() {
 
-		$post_type  = 'post';
-		$meta_key   = 'test_key';
-		$meta_value = 'Test Value';
+	// 	$post_type  = 'post';
+	// 	$meta_key   = 'test_key';
+	// 	$meta_value = 'Test Value';
 
-		// Create a post.
-		$post = $this->factory->post->create( array( 'post_title' => 'Test Post' ) );
+	// 	// Create a post.
+	// 	$post = $this->factory->post->create( array( 'post_title' => 'Test Post' ) );
 
-		$comment_data = array(
-			'comment_post_ID' => $post,
-			'comment_content' => 'Test Comment',
-		);
+	// 	$comment_data = array(
+	// 		'comment_post_ID' => $post,
+	// 		'comment_content' => 'Test Comment',
+	// 	);
 
-		// Create a comment.
-		$comment_id = $this->factory->comment->create( $comment_data );
+	// 	// Create a comment.
+	// 	$comment_id = $this->factory->comment->create( $comment_data );
 
-		add_comment_meta( $comment_id, $meta_key, $meta_value );
+	// 	add_comment_meta( $comment_id, $meta_key, $meta_value );
 
-		// call our method.
-		$delete_options = array(
-			'post_type' => $post_type,
-			'use_value' => false,
-			'meta_key'  => $meta_key,
-			'limit_to'  => -1,
-			'date_op'   => '',
-			'days'      => '',
-			'restrict'  => false,
-		);
+	// 	// call our method.
+	// 	$delete_options = array(
+	// 		'post_type' => $post_type,
+	// 		'use_value' => false,
+	// 		'meta_key'  => $meta_key,
+	// 		'limit_to'  => -1,
+	// 		'date_op'   => '',
+	// 		'days'      => '',
+	// 		'restrict'  => false,
+	// 	);
 
-		$meta_deleted = $this->module->delete( $delete_options );
-		$this->assertEquals( 1, $meta_deleted );
+	// 	$meta_deleted = $this->module->delete( $delete_options );
+	// 	$this->assertEquals( 1, $meta_deleted );
 
-		$comment_meta = get_comment_meta( $comment_id, $meta_key );
-		$this->assertEquals( 0, count( $comment_meta ) );
-	}
+	// 	$comment_meta = get_comment_meta( $comment_id, $meta_key );
+	// 	$this->assertEquals( 0, count( $comment_meta ) );
+	// }
 
-	/**
-	 * Add to test deleting comment meta from more than one comment.
-	 */
-	public function test_that_deleting_comment_meta_from_more_than_one_comment() {
+	// /**
+	//  * Add to test deleting comment meta from more than one comment.
+	//  */
+	// public function test_that_deleting_comment_meta_from_more_than_one_comment() {
 
-		$post_type  = 'post';
-		$meta_key   = 'test_key';
-		$meta_value = 'Test Value';
+	// 	$post_type  = 'post';
+	// 	$meta_key   = 'test_key';
+	// 	$meta_value = 'Test Value';
 
-		// Create a post.
-		$post = $this->factory->post->create( array( 'post_title' => 'Test Post' ) );
+	// 	// Create a post.
+	// 	$post = $this->factory->post->create( array( 'post_title' => 'Test Post' ) );
 
-		$comment_data = array(
-			'comment_post_ID' => $post,
-			'comment_content' => 'Test Comment',
-		);
+	// 	$comment_data = array(
+	// 		'comment_post_ID' => $post,
+	// 		'comment_content' => 'Test Comment',
+	// 	);
 
-		// Create a comment.
-		$comment_id_1 = $this->factory->comment->create( $comment_data );
+	// 	// Create a comment.
+	// 	$comment_id_1 = $this->factory->comment->create( $comment_data );
 
-		add_comment_meta( $comment_id_1, $meta_key, $meta_value );
+	// 	add_comment_meta( $comment_id_1, $meta_key, $meta_value );
 
-		// Create a comment.
-		$comment_id_2 = $this->factory->comment->create( $comment_data );
+	// 	// Create a comment.
+	// 	$comment_id_2 = $this->factory->comment->create( $comment_data );
 
-		add_comment_meta( $comment_id_2, $meta_key, $meta_value );
+	// 	add_comment_meta( $comment_id_2, $meta_key, $meta_value );
 
-		// Create a comment.
-		$comment_id_3 = $this->factory->comment->create( $comment_data );
+	// 	// Create a comment.
+	// 	$comment_id_3 = $this->factory->comment->create( $comment_data );
 
-		add_comment_meta( $comment_id_3, $meta_key, $meta_value );
+	// 	add_comment_meta( $comment_id_3, $meta_key, $meta_value );
 
-		// call our method.
-		$delete_options = array(
-			'post_type' => $post_type,
-			'use_value' => false,
-			'meta_key'  => $meta_key,
-			'limit_to'  => -1,
-			'date_op'   => '',
-			'days'      => '',
-			'restrict'  => false,
-		);
+	// 	// call our method.
+	// 	$delete_options = array(
+	// 		'post_type' => $post_type,
+	// 		'use_value' => false,
+	// 		'meta_key'  => $meta_key,
+	// 		'limit_to'  => -1,
+	// 		'date_op'   => '',
+	// 		'days'      => '',
+	// 		'restrict'  => false,
+	// 	);
 
-		$meta_deleted = $this->module->delete( $delete_options );
-		$this->assertEquals( 3, $meta_deleted );
+	// 	$meta_deleted = $this->module->delete( $delete_options );
+	// 	$this->assertEquals( 3, $meta_deleted );
 
-	}
+	// }
 
-	/**
-	 * Add to test deleting comment meta from one comment using meta value as well.
-	 */
-	public function test_that_deleting_comment_meta_from_one_comment_using_meta_value() {
+	// /**
+	//  * Add to test deleting comment meta from one comment using meta value as well.
+	//  */
+	// public function test_that_deleting_comment_meta_from_one_comment_using_meta_value() {
 
-		$post_type  = 'post';
-		$meta_key   = 'test_key';
-		$meta_value = 'Test Value';
+	// 	$post_type  = 'post';
+	// 	$meta_key   = 'test_key';
+	// 	$meta_value = 'Test Value';
 
-		// Create a post.
-		$post = $this->factory->post->create( array( 'post_title' => 'Test Post' ) );
+	// 	// Create a post.
+	// 	$post = $this->factory->post->create( array( 'post_title' => 'Test Post' ) );
 
-		$comment_data = array(
-			'comment_post_ID' => $post,
-			'comment_content' => 'Test Comment',
-		);
+	// 	$comment_data = array(
+	// 		'comment_post_ID' => $post,
+	// 		'comment_content' => 'Test Comment',
+	// 	);
 
-		// Create a comment.
-		$comment_id = $this->factory->comment->create( $comment_data );
+	// 	// Create a comment.
+	// 	$comment_id = $this->factory->comment->create( $comment_data );
 
-		add_comment_meta( $comment_id, $meta_key, $meta_value );
+	// 	add_comment_meta( $comment_id, $meta_key, $meta_value );
 
-		// call our method.
-		$delete_options = array(
-			'post_type' 	=> $post_type,
-			'use_value' 	=> true,
-			'meta_key'  	=> $meta_key,
-			'meta_value'  	=> $meta_value,
-			'meta_op'  		=> '=',
-			'meta_type'  	=> 'CHAR',
-			'limit_to'  	=> -1,
-			'date_op'   	=> '',
-			'days'      	=> '',
-			'restrict'  	=> false,
-		);
+	// 	// call our method.
+	// 	$delete_options = array(
+	// 		'post_type' 	=> $post_type,
+	// 		'use_value' 	=> true,
+	// 		'meta_key'  	=> $meta_key,
+	// 		'meta_value'  	=> $meta_value,
+	// 		'meta_op'  		=> '=',
+	// 		'meta_type'  	=> 'CHAR',
+	// 		'limit_to'  	=> -1,
+	// 		'date_op'   	=> '',
+	// 		'days'      	=> '',
+	// 		'restrict'  	=> false,
+	// 	);
 
-		$meta_deleted = $this->module->delete( $delete_options );
-		$this->assertEquals( 1, $meta_deleted );
+	// 	$meta_deleted = $this->module->delete( $delete_options );
+	// 	$this->assertEquals( 1, $meta_deleted );
 
-		$comment_meta = get_comment_meta( $comment_id, $meta_key );
-		$this->assertEquals( 0, count( $comment_meta ) );
-	}
+	// 	$comment_meta = get_comment_meta( $comment_id, $meta_key );
+	// 	$this->assertEquals( 0, count( $comment_meta ) );
+	// }
 
 	/**
 	 * Add to test deleting comment meta from more than one comment using meta value as well.
@@ -195,15 +195,16 @@ class DeleteCommentMetaModuleTest extends WPCoreUnitTestCase {
 		// call our method.
 		$delete_options = array(
 			'post_type' 	=> $post_type,
-			'use_value' 	=> true,
+			'use_value' 	=> 1,
 			'meta_key'  	=> $meta_key,
 			'meta_value'  	=> $meta_value,
 			'meta_op'  		=> '=',
 			'meta_type'  	=> 'CHAR',
-			'limit_to'  	=> -1,
+			'limit_to'  	=> 0,
 			'date_op'   	=> '',
-			'days'      	=> '',
+			'days'      	=> 0,
 			'restrict'  	=> false,
+			'force_delete'	=> false,
 		);
 
 		$meta_deleted = $this->module->delete( $delete_options );
@@ -212,79 +213,70 @@ class DeleteCommentMetaModuleTest extends WPCoreUnitTestCase {
 	}
 
 	/**
-	 * Add to test deleting comment meta from more than one comment using meta value as well with different operations.
+	 * Data provider to test `meta_value_with_different_operations` method.
+	 *
+	 * @return array Data.
 	 */
-	public function test_that_deleting_comment_meta_from_more_than_one_comment_using_meta_value_as_well_with_different_operations() {
+	public function different_operations(){
+		return array(
+			array(
+				'meta_type'	 => 'CHAR',
+				'meta_key'   => 'test_key',
+				'meta_value' => 'Test Value',
+				'operator'		=> '=',
+				'query_value'	=> 'Test Value',
+			),
+			array(
+				'meta_type'	 => 'CHAR',
+				'meta_key'   => 'test_key',
+				'meta_value' => 'Test Value',
+				'operator'		=> '!=',
+				'query_value'	=> 'Test Value',
+			),
+		);
+	}
+
+	/**
+	 * Add to test deleting comment meta from more than one comment using meta value as well with different operations.
+	 *
+	 * @dataProvider different_operations
+	 */
+	public function test_that_deleting_comment_meta_more_than_one_comment_using_meta_value_with_different_operations( $meta_type, $meta_key, $meta_value, $operator, $query_value ) {
 
 		$post_type  = 'post';
 		$number_of_comments = 10;
-		$values = array(
-			'CHAR' => array(
-				'meta_key'   => 'test_key',
-				'meta_value' => 'Test Value',
-				'valid_op'	 => array(
-					'=',
-					'!=',
-					'LIKE',
-					'NOT LIKE',
-				),
-			),
-			'NUMERIC' => array(
-				'meta_key'   => 'test_key',
-				'meta_value' => '10',
-				'valid_op'	 => array(
-					'=',
-					'!=',
-					'<',
-					'<=',
-					'>',
-					'>=',
-				),
-			),
-		);
 
-		// Create a post.
+		//Create a post.
 		$post = $this->factory->post->create( array( 'post_title' => 'Test Post' ) );
 
 		for( $i = 1; $i <= $number_of_comments; $i++ ){
 
-			foreach( $values as $value ){
-				
-				$comment_data = array(
-					'comment_post_ID' => $post,
-					'comment_content' => 'Test Comment',
-				);
+			$comment_data = array(
+				'comment_post_ID' => $post,
+				'comment_content' => 'Test Comment',
+			);
 
-				$comment_id = $this->factory->comment->create( $comment_data );
-				add_comment_meta( $comment_id, $value['meta_key'], $value['meta_value'] );
-			}
+			$comment_id = $this->factory->comment->create( $comment_data );
+			add_comment_meta( $comment_id, $meta_key, $meta_value );
 
 		}
 
-		foreach( $values as $key => $value ){
+		$delete_options = array(
+			'post_type' 	=> $post_type,
+			'use_value' 	=> true,
+			'meta_key'  	=> $meta_key,
+			'meta_value'  	=> $query_value,
+			'meta_op'  		=> $operator,
+			'meta_type'  	=> $meta_type,
+			'limit_to'  	=> -1,
+			'date_op'   	=> '',
+			'days'      	=> '',
+			'restrict'  	=> false,
+		);
 
-			foreach( $value['valid_op'] as $valid_op_value ){
-
-				$delete_options = array(
-					'post_type' 	=> $post_type,
-					'use_value' 	=> true,
-					'meta_key'  	=> $value['meta_key'],
-					'meta_value'  	=> $value['meta_value'],
-					'meta_op'  		=> $valid_op_value,
-					'meta_type'  	=> $key,
-					'limit_to'  	=> -1,
-					'date_op'   	=> '',
-					'days'      	=> '',
-					'restrict'  	=> false,
-				);
-
-				$meta_deleted = $this->module->delete( $delete_options );
-				$this->assertEquals( $number_of_comments, $meta_deleted );
-
-			}
+		$meta_deleted = $this->module->delete( $delete_options );
+		$this->assertEquals( $number_of_comments, $meta_deleted );
 			
-		}
-
 	}
 
 }
