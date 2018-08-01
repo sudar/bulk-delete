814175a4d3bc21664f652fe8084ac44ba93c8138
Rajan <rajanit2000@gmail.com>
2018-08-01 07:44:50 +0530

    Add date filters Fix #331

--- tests/wp-unit/include/Core/Metas/Modules/DeleteCommentMetaModuleTest.php
+++ tests/wp-unit/include/Core/Metas/Modules/DeleteCommentMetaModuleTest.php
@@ -299,4 +299,88 @@ class DeleteCommentMetaModuleTest extends WPCoreUnitTestCase {
 
 	}
 
+	/**
+	 * Add to test deleting comment meta older than x days.
+	 */
+	public function test_that_deleting_comment_meta_older_than_x_days() {
+
+		$post_type  = 'post';
+		$meta_key   = 'test_key';
+		$meta_value = 'Test Value';
+		$day_post   = date( 'Y-m-d H:i:s', strtotime( '-2 day' ) );
+
+		// Create a post.
+		$post = $this->factory->post->create( array( 'post_title' => 'Test Post' ) );
+
+		$comment_data = array(
+			'comment_post_ID' => $post,
+			'comment_content' => 'Test Comment',
+			'comment_date'    => $day_post,
+		);
+
+		// Create a comment.
+		$comment_id = $this->factory->comment->create( $comment_data );
+
+		add_comment_meta( $comment_id, $meta_key, $meta_value );
+
+		// call our method.
+		$delete_options = array(
+			'post_type' => $post_type,
+			'use_value' => false,
+			'meta_key'  => $meta_key,
+			'limit_to'  => -1,
+			'date_op'   => 'before',
+			'days'      => 1,
+			'restrict'  => true,
+		);
+
+		$meta_deleted = $this->module->delete( $delete_options );
+		$this->assertEquals( 1, $meta_deleted );
+
+		$comment_meta = get_comment_meta( $comment_id, $meta_key );
+		$this->assertEquals( 0, count( $comment_meta ) );
+	}
+
+	/**
+	 * Add to test deleting comment meta older than x days.
+	 */
+	public function test_that_deleting_comment_meta_last_x_days() {
+
+		$post_type  = 'post';
+		$meta_key   = 'test_key';
+		$meta_value = 'Test Value';
+		$day_post   = date( 'Y-m-d H:i:s', strtotime( '-3 day' ) );
+
+		// Create a post.
+		$post = $this->factory->post->create( array( 'post_title' => 'Test Post' ) );
+
+		$comment_data = array(
+			'comment_post_ID' => $post,
+			'comment_content' => 'Test Comment',
+			'comment_date'    => $day_post,
+		);
+
+		// Create a comment.
+		$comment_id = $this->factory->comment->create( $comment_data );
+
+		add_comment_meta( $comment_id, $meta_key, $meta_value );
+
+		// call our method.
+		$delete_options = array(
+			'post_type' => $post_type,
+			'use_value' => false,
+			'meta_key'  => $meta_key,
+			'limit_to'  => -1,
+			'date_op'   => 'after',
+			'days'      => '5',
+			'restrict'  => true,
+		);
+
+		$meta_deleted = $this->module->delete( $delete_options );
+		$this->assertEquals( 1, $meta_deleted );
+
+		$comment_meta = get_comment_meta( $comment_id, $meta_key );
+		$this->assertEquals( 0, count( $comment_meta ) );
+	}
+
 }
