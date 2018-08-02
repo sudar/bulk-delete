784e3a009425af01210c54fc21928ef00e33b800
Rajan <rajanit2000@gmail.com>
2018-08-03 02:29:18 +0530

    Refactor Delete term by post count test module Fix #178

--- tests/wp-unit/include/Core/Terms/Modules/DeleteTermsByPostCountModuleTest.php
+++ tests/wp-unit/include/Core/Terms/Modules/DeleteTermsByPostCountModuleTest.php
@@ -30,236 +30,425 @@ class DeleteTermsByPostCountModuleTest extends WPCoreUnitTestCase {
 	}
 
 	/**
-	 * Add tests to delete term with equal state.
+	 * Dataprovider of test case.
 	 */
-	public function test_that_delete_terms_with_equal_state() {
-
-		$post_type = 'movie';
-
-		$this->register_post_type_and_associate_category( $post_type );
-
-		// Create two categories.
-		$cat1 = $this->factory->category->create( array( 'name' => 'cat1' ) );
-
-		// Create two posts and assign them to categories just created.
-		$post1 = $this->factory->post->create(
+	public function delete_terms_by_post_count_various_inputs() {
+		return array(
 			array(
-				'post_title' => 'post1',
-				'post_type'  => $post_type,
-			)
-		);
-		$post2 = $this->factory->post->create(
+				array(
+					'post_type'  => 'post',
+					'taxonomy'   => 'category',
+					'term_input' => array(
+						array(
+							'term'            => 'Term A',
+							'number_0f_posts' => 5,
+						),
+						array(
+							'term'            => 'Term B',
+							'number_0f_posts' => 2,
+						),
+						array(
+							'term'            => 'Term C',
+							'number_0f_posts' => 0,
+						),
+					),
+				),
+				array(
+					'post_count' => 5,
+					'operator'   => 'equal_to',
+				),
+				1,
+			),
+
 			array(
-				'post_title' => 'post2',
-				'post_type'  => $post_type,
-			)
-		);
-		$post3 = $this->factory->post->create(
+				array(
+					'post_type'  => 'post',
+					'taxonomy'   => 'category',
+					'term_input' => array(
+						array(
+							'term'            => 'Term A',
+							'number_0f_posts' => 5,
+						),
+						array(
+							'term'            => 'Term B',
+							'number_0f_posts' => 5,
+						),
+						array(
+							'term'            => 'Term C',
+							'number_0f_posts' => 0,
+						),
+					),
+				),
+				array(
+					'post_count' => 5,
+					'operator'   => 'equal_to',
+				),
+				2,
+			),
+
 			array(
-				'post_title' => 'post3',
-				'post_type'  => $post_type,
-			)
+				array(
+					'post_type'  => 'post',
+					'taxonomy'   => 'category',
+					'term_input' => array(
+						array(
+							'term'            => 'Term A',
+							'number_0f_posts' => 5,
+						),
+						array(
+							'term'            => 'Term B',
+							'number_0f_posts' => 5,
+						),
+						array(
+							'term'            => 'Term C',
+							'number_0f_posts' => 5,
+						),
+					),
+				),
+				array(
+					'post_count' => 2,
+					'operator'   => 'equal_to',
+				),
+				0,
+			),
+
+			array(
+				array(
+					'post_type'  => 'post',
+					'taxonomy'   => 'category',
+					'term_input' => array(
+						array(
+							'term'            => 'Term A',
+							'number_0f_posts' => 3,
+						),
+						array(
+							'term'            => 'Term B',
+							'number_0f_posts' => 2,
+						),
+						array(
+							'term'            => 'Term C',
+							'number_0f_posts' => 0,
+						),
+					),
+				),
+				array(
+					'post_count' => 3,
+					'operator'   => 'not_equal_to',
+				),
+				3, // Because Uncategorized is default term for category taxonomy, Its also does not have posts.
+			),
+
+			array(
+				array(
+					'post_type'  => 'post',
+					'taxonomy'   => 'category',
+					'term_input' => array(
+						array(
+							'term'            => 'Term A',
+							'number_0f_posts' => 3,
+						),
+						array(
+							'term'            => 'Term B',
+							'number_0f_posts' => 3,
+						),
+						array(
+							'term'            => 'Term C',
+							'number_0f_posts' => 0,
+						),
+					),
+				),
+				array(
+					'post_count' => 3,
+					'operator'   => 'not_equal_to',
+				),
+				2, // Because Uncategorized is default term for category taxonomy, Its also does not have posts.
+			),
+
+			array(
+				array(
+					'post_type'  => 'post',
+					'taxonomy'   => 'category',
+					'term_input' => array(
+						array(
+							'term'            => 'Term A',
+							'number_0f_posts' => 3,
+						),
+						array(
+							'term'            => 'Term B',
+							'number_0f_posts' => 3,
+						),
+						array(
+							'term'            => 'Term C',
+							'number_0f_posts' => 3,
+						),
+					),
+				),
+				array(
+					'post_count' => 3,
+					'operator'   => 'not_equal_to',
+				),
+				1, // Because Uncategorized is default term for category taxonomy, Its also does not have posts.
+			),
+
+			array(
+				array(
+					'post_type'  => 'post',
+					'taxonomy'   => 'category',
+					'term_input' => array(
+						array(
+							'term'            => 'Term A',
+							'number_0f_posts' => 3,
+						),
+						array(
+							'term'            => 'Term B',
+							'number_0f_posts' => 3,
+						),
+						array(
+							'term'            => 'Term C',
+							'number_0f_posts' => 3,
+						),
+					),
+				),
+				array(
+					'post_count' => 0,
+					'operator'   => 'not_equal_to',
+				),
+				3, // Because Uncategorized is default term for category taxonomy, Its also does not have posts.
+			),
+
+			array(
+				array(
+					'post_type'  => 'post',
+					'taxonomy'   => 'category',
+					'term_input' => array(
+						array(
+							'term'            => 'Term A',
+							'number_0f_posts' => 5,
+						),
+						array(
+							'term'            => 'Term B',
+							'number_0f_posts' => 4,
+						),
+						array(
+							'term'            => 'Term C',
+							'number_0f_posts' => 3,
+						),
+					),
+				),
+				array(
+					'post_count' => 4,
+					'operator'   => 'less_than',
+				),
+				2, // Because Uncategorized is default term for category taxonomy, Its also does not have posts.
+			),
+
+			array(
+				array(
+					'post_type'  => 'post',
+					'taxonomy'   => 'category',
+					'term_input' => array(
+						array(
+							'term'            => 'Term A',
+							'number_0f_posts' => 2,
+						),
+						array(
+							'term'            => 'Term B',
+							'number_0f_posts' => 2,
+						),
+						array(
+							'term'            => 'Term C',
+							'number_0f_posts' => 3,
+						),
+					),
+				),
+				array(
+					'post_count' => 4,
+					'operator'   => 'less_than',
+				),
+				4, // Because Uncategorized is default term for category taxonomy, Its also does not have posts.
+			),
+
+			array(
+				array(
+					'post_type'  => 'post',
+					'taxonomy'   => 'category',
+					'term_input' => array(
+						array(
+							'term'            => 'Term A',
+							'number_0f_posts' => 3,
+						),
+						array(
+							'term'            => 'Term B',
+							'number_0f_posts' => 3,
+						),
+						array(
+							'term'            => 'Term C',
+							'number_0f_posts' => 3,
+						),
+					),
+				),
+				array(
+					'post_count' => 2,
+					'operator'   => 'less_than',
+				),
+				1, // Because Uncategorized is default term for category taxonomy, Its also does not have posts.
+			),
+
+			array(
+				array(
+					'post_type'  => 'post',
+					'taxonomy'   => 'category',
+					'term_input' => array(
+						array(
+							'term'            => 'Term A',
+							'number_0f_posts' => 5,
+						),
+						array(
+							'term'            => 'Term B',
+							'number_0f_posts' => 4,
+						),
+						array(
+							'term'            => 'Term C',
+							'number_0f_posts' => 3,
+						),
+					),
+				),
+				array(
+					'post_count' => 4,
+					'operator'   => 'greater_than',
+				),
+				1,
+			),
+
+			array(
+				array(
+					'post_type'  => 'post',
+					'taxonomy'   => 'category',
+					'term_input' => array(
+						array(
+							'term'            => 'Term A',
+							'number_0f_posts' => 5,
+						),
+						array(
+							'term'            => 'Term B',
+							'number_0f_posts' => 6,
+						),
+						array(
+							'term'            => 'Term C',
+							'number_0f_posts' => 3,
+						),
+					),
+				),
+				array(
+					'post_count' => 4,
+					'operator'   => 'greater_than',
+				),
+				2,
+			),
+
+			array(
+				array(
+					'post_type'  => 'post',
+					'taxonomy'   => 'category',
+					'term_input' => array(
+						array(
+							'term'            => 'Term A',
+							'number_0f_posts' => 2,
+						),
+						array(
+							'term'            => 'Term B',
+							'number_0f_posts' => 3,
+						),
+						array(
+							'term'            => 'Term C',
+							'number_0f_posts' => 3,
+						),
+					),
+				),
+				array(
+					'post_count' => 4,
+					'operator'   => 'greater_than',
+				),
+				0,
+			),
+
+			array(
+				array(
+					'post_type'  => 'custom_post_type',
+					'taxonomy'   => 'custom_taxonomy',
+					'term_input' => array(
+						array(
+							'term'            => 'Term A',
+							'number_0f_posts' => 5,
+						),
+						array(
+							'term'            => 'Term B',
+							'number_0f_posts' => 2,
+						),
+						array(
+							'term'            => 'Term C',
+							'number_0f_posts' => 0,
+						),
+					),
+				),
+				array(
+					'post_count' => 5,
+					'operator'   => 'equal_to',
+				),
+				1,
+			),
 		);
-
-		wp_set_object_terms( $post1, $cat1, 'category' );
-		wp_set_object_terms( $post2, $cat1, 'category' );
-		wp_set_object_terms( $post3, $cat1, 'category' );
-
-		// Assert that each category has one post.
-		$posts_in_cat1 = $this->get_posts_by_custom_term( $cat1, 'category', $post_type );
-
-		$this->assertEquals( 3, count( $posts_in_cat1 ) );
-
-		// call our method.
-		$delete_options = array(
-			'post_type' => array( $post_type ),
-			'taxonomy'  => 'category',
-			'term_opt'  => 'equal_to',
-			'term_text' => '3',
-		);
-		$this->module->delete( $delete_options );
-
-		$posts_in_cat1 = $this->get_posts_by_custom_term( $cat1, 'category', $post_type );
-
-		$this->assertEquals( 0, count( $posts_in_cat1 ) );
-
 	}
 
 	/**
-	 * Add tests to delete term with not equal state.
-	 */
-	public function test_that_delete_terms_with_not_equal_state() {
-
-		$post_type = 'movie';
-
-		$this->register_post_type_and_associate_category( $post_type );
-
-		// Create two categories.
-		$cat1 = $this->factory->category->create( array( 'name' => 'cat1' ) );
-
-		// Create two posts and assign them to categories just created.
-		$post1 = $this->factory->post->create(
-			array(
-				'post_title' => 'post1',
-				'post_type'  => $post_type,
-			)
-		);
-		$post2 = $this->factory->post->create(
-			array(
-				'post_title' => 'post2',
-				'post_type'  => $post_type,
-			)
-		);
-		$post3 = $this->factory->post->create(
-			array(
-				'post_title' => 'post3',
-				'post_type'  => $post_type,
-			)
-		);
-
-		wp_set_object_terms( $post1, $cat1, 'category' );
-		wp_set_object_terms( $post2, $cat1, 'category' );
-		wp_set_object_terms( $post3, $cat1, 'category' );
-
-		// Assert that each category has one post.
-		$posts_in_cat1 = $this->get_posts_by_custom_term( $cat1, 'category', $post_type );
-
-		$this->assertEquals( 3, count( $posts_in_cat1 ) );
-
-		// call our method.
-		$delete_options = array(
-			'post_type' => array( $post_type ),
-			'taxonomy'  => 'category',
-			'term_opt'  => 'not_equal_to',
-			'term_text' => '5',
-		);
-		$this->module->delete( $delete_options );
-
-		$posts_in_cat1 = $this->get_posts_by_custom_term( $cat1, 'category', $post_type );
-
-		$this->assertEquals( 0, count( $posts_in_cat1 ) );
-
-	}
-
-	/**
-	 * Add tests to delete term with less than state.
-	 */
-	public function test_that_delete_terms_with_less_than_state() {
-
-		$post_type = 'movie';
-
-		$this->register_post_type_and_associate_category( $post_type );
-
-		// Create two categories.
-		$cat1 = $this->factory->category->create( array( 'name' => 'cat1' ) );
-
-		// Create two posts and assign them to categories just created.
-		$post1 = $this->factory->post->create(
-			array(
-				'post_title' => 'post1',
-				'post_type'  => $post_type,
-			)
-		);
-		$post2 = $this->factory->post->create(
-			array(
-				'post_title' => 'post2',
-				'post_type'  => $post_type,
-			)
-		);
-		$post3 = $this->factory->post->create(
-			array(
-				'post_title' => 'post3',
-				'post_type'  => $post_type,
-			)
-		);
-
-		wp_set_object_terms( $post1, $cat1, 'category' );
-		wp_set_object_terms( $post2, $cat1, 'category' );
-		wp_set_object_terms( $post3, $cat1, 'category' );
-
-		// Assert that each category has one post.
-		$posts_in_cat1 = $this->get_posts_by_custom_term( $cat1, 'category', $post_type );
-
-		$this->assertEquals( 3, count( $posts_in_cat1 ) );
-
-		// call our method.
-		$delete_options = array(
-			'post_type' => array( $post_type ),
-			'taxonomy'  => 'category',
-			'term_opt'  => 'less_than',
-			'term_text' => '5',
-		);
-		$this->module->delete( $delete_options );
-
-		$posts_in_cat1 = $this->get_posts_by_custom_term( $cat1, 'category', $post_type );
-
-		$this->assertEquals( 0, count( $posts_in_cat1 ) );
-
-	}
-
-	/**
-	 * Add tests to delete term with greater than state.
-	 */
-	public function test_that_delete_terms_with_greater_than_state() {
-
-		$post_type = 'movie';
-
-		$this->register_post_type_and_associate_category( $post_type );
-
-		// Create two categories.
-		$cat1 = $this->factory->category->create( array( 'name' => 'cat1' ) );
-
-		// Create two posts and assign them to categories just created.
-		$post1 = $this->factory->post->create(
-			array(
-				'post_title' => 'post1',
-				'post_type'  => $post_type,
-			)
-		);
-		$post2 = $this->factory->post->create(
-			array(
-				'post_title' => 'post2',
-				'post_type'  => $post_type,
-			)
-		);
-		$post3 = $this->factory->post->create(
-			array(
-				'post_title' => 'post3',
-				'post_type'  => $post_type,
-			)
-		);
-
-		wp_set_object_terms( $post1, $cat1, 'category' );
-		wp_set_object_terms( $post2, $cat1, 'category' );
-		wp_set_object_terms( $post3, $cat1, 'category' );
-
-		// Assert that each category has one post.
-		$posts_in_cat1 = $this->get_posts_by_custom_term( $cat1, 'category', $post_type );
-
-		$this->assertEquals( 3, count( $posts_in_cat1 ) );
-
-		// call our method.
-		$delete_options = array(
-			'post_type' => array( $post_type ),
-			'taxonomy'  => 'category',
-			'term_opt'  => 'greater_than',
-			'term_text' => '2',
-		);
-		$this->module->delete( $delete_options );
-
-		$posts_in_cat1 = $this->get_posts_by_custom_term( $cat1, 'category', $post_type );
-
-		$this->assertEquals( 0, count( $posts_in_cat1 ) );
-
-	}
-
-	/**
-	 * Register a custom post type and then associate `category` taxonomy to it.
+	 * Add tests to delete term with various cases.
 	 *
-	 * @param string $post_type Post type name.
+	 * @dataProvider delete_terms_by_post_count_various_inputs
+	 *
+	 * @param array $inputs Inputs for test cases.
+	 * @param array $operation Operation performed after user inputs.
+	 * @param int   $expected Expected value.
 	 */
-	protected function register_post_type_and_associate_category( $post_type ) {
+	public function test_that_delete_terms_by_post_count( $inputs, $operation, $expected ) {
+
+		$post_type  = $inputs['post_type'];
+		$taxonomy   = $inputs['taxonomy'];
+		$term_input = $inputs['term_input'];
+
+		$default_taxonomies = array( 'category', 'post_tag', 'link_category', 'post_format' );
+
 		register_post_type( $post_type );
-		register_taxonomy_for_object_type( 'category', $post_type );
+		if ( ! in_array( $taxonomy, $default_taxonomies, true ) ) {
+			register_taxonomy( $taxonomy, $post_type );
+		}
+		register_taxonomy_for_object_type( $taxonomy, $post_type );
+
+		foreach ( $term_input as $term ) {
+			$matched_term_array = wp_insert_term( $term['term'], $taxonomy );
+			for ( $i = 0; $i < $term['number_0f_posts']; $i++ ) {
+
+				$post = $this->factory->post->create(
+					array(
+						'post_type' => $post_type,
+					)
+				);
+
+				wp_set_object_terms( $post, $matched_term_array, $taxonomy );
+			}
+		}
+
+		// call our method.
+		$delete_options = array(
+			'post_type' => array( $post_type ),
+			'taxonomy'  => $taxonomy,
+			'term_opt'  => $operation['operator'],
+			'term_text' => $operation['post_count'],
+		);
+		$deleted_term   = $this->module->delete( $delete_options );
+
+		$this->assertEquals( $expected, $deleted_term );
+
 	}
+
+
 }
