4ef178b20416fc6ef67e1e67e586f50f9039f9d1
Rajan <rajanit2000@gmail.com>
2018-08-16 04:01:28 +0530

    Update review changes Fix #353

--- tests/wp-unit/include/Core/Posts/Modules/DeletePostsByRevisionModuleTest.php
+++ tests/wp-unit/include/Core/Posts/Modules/DeletePostsByRevisionModuleTest.php
@@ -30,9 +30,9 @@ class DeletePostsByRevisionModuleTest extends WPCoreUnitTestCase {
 	}
 
 	/**
-	 * Test trash revisions for a single post
+	 * Test thet post revisions can be deleted form a single post
 	 */
-	public function test_trash_revisions_for_single_post() {
+	public function test_that_post_revisions_can_be_deleted_from_single_post() {
 
 		$post_id = $this->factory->post->create(
 			array(
@@ -57,66 +57,20 @@ class DeletePostsByRevisionModuleTest extends WPCoreUnitTestCase {
 		wp_update_post( $revision_post_2 );
 
 		$delete_options = array(
-			'revisions'    => 'revisions',
-			'force_delete' => true,
+			'revisions' => 'revisions',
 		);
 
 		$posts_deleted = $this->module->delete( $delete_options );
 		$this->assertEquals( 2, $posts_deleted );
-
-		$trash_posts = $this->get_posts_by_status( 'trash' );
-		$this->assertEquals( 0, count( $trash_posts ) );
 
 		$post = get_post( $post_id );
 		$this->assertEquals( $post->ID, $post_id );
 	}
 
 	/**
-	 * Test delete revisions for a single post
+	 * Test thet post revisions can be deleted form multiple post
 	 */
-	public function test_delete_revisions_for_single_post() {
-
-		$post_id = $this->factory->post->create(
-			array(
-				'post_type' => 'post',
-			)
-		);
-
-		$revision_post_1 = array(
-			'ID'           => $post_id,
-			'post_title'   => rand(),
-			'post_content' => md5( rand() ),
-		);
-
-		wp_update_post( $revision_post_1 );
-
-		$revision_post_2 = array(
-			'ID'           => $post_id,
-			'post_title'   => rand(),
-			'post_content' => md5( rand() ),
-		);
-
-		wp_update_post( $revision_post_2 );
-
-		$delete_options = array(
-			'revisions'    => 'revisions',
-			'force_delete' => true,
-		);
-
-		$posts_deleted = $this->module->delete( $delete_options );
-		$this->assertEquals( 2, $posts_deleted );
-
-		$trash_posts = $this->get_posts_by_status( 'trash' );
-		$this->assertEquals( 0, count( $trash_posts ) );
-
-		$post = get_post( $post_id );
-		$this->assertEquals( $post->ID, $post_id );
-	}
-
-	/**
-	 * Test trash revisions for a multiple post
-	 */
-	public function test_trash_revisions_for_multiple_post() {
+	public function test_that_post_revisions_can_be_deleted_from_multiple_post() {
 
 		$post_ids = $this->factory->post->create_many(
 			10, array(
@@ -137,56 +91,11 @@ class DeletePostsByRevisionModuleTest extends WPCoreUnitTestCase {
 		}
 
 		$delete_options = array(
-			'revisions'    => 'revisions',
-			'force_delete' => true,
+			'revisions' => 'revisions',
 		);
 
 		$posts_deleted = $this->module->delete( $delete_options );
 		$this->assertEquals( 10, $posts_deleted );
-
-		$trash_posts = $this->get_posts_by_status( 'trash' );
-		$this->assertEquals( 0, count( $trash_posts ) );
-
-		foreach ( $post_ids as $post_id ) {
-			$post = get_post( $post_id );
-			$this->assertEquals( $post->ID, $post_id );
-		}
-
-	}
-
-	/**
-	 * Test delete revisions for a multiple post
-	 */
-	public function test_delete_revisions_for_multiple_post() {
-
-		$post_ids = $this->factory->post->create_many(
-			10, array(
-				'post_type' => 'post',
-			)
-		);
-
-		foreach ( $post_ids as $post_id ) {
-
-			$revision_post = array(
-				'ID'           => $post_id,
-				'post_title'   => rand(),
-				'post_content' => md5( rand() ),
-			);
-
-			wp_update_post( $revision_post );
-
-		}
-
-		$delete_options = array(
-			'revisions'    => 'revisions',
-			'force_delete' => true,
-		);
-
-		$posts_deleted = $this->module->delete( $delete_options );
-		$this->assertEquals( 10, $posts_deleted );
-
-		$trash_posts = $this->get_posts_by_status( 'trash' );
-		$this->assertEquals( 0, count( $trash_posts ) );
 
 		foreach ( $post_ids as $post_id ) {
 			$post = get_post( $post_id );
