fdf2c767401ca446ed6c9ebd19415a4e3adf1bd9
Maria Daniel Deepak <mariadanieldeepak@gmail.com>
2018-09-19 18:45:52 +0530

    Show helper text based on the Scheduler plugin activation status

--- include/Core/Posts/Modules/DeletePostsByPostTypeModule.php
+++ include/Core/Posts/Modules/DeletePostsByPostTypeModule.php
@@ -42,7 +42,9 @@ class DeletePostsByPostTypeModule extends PostsModule {
 				$this->render_restrict_settings();
 				$this->render_delete_settings();
 				$this->render_limit_settings();
-				$this->render_cron_settings();
+
+				$scheduler_plugin = 'bulk-delete-scheduler-for-deleting-posts-by-post-type/bulk-delete-scheduler-for-deleting-posts-by-post-type.php';
+				$this->render_cron_settings( is_plugin_active( $scheduler_plugin ) );
 				?>
 
 			</table>
