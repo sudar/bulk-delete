aa3d2cdfa0360bd6f555751440078138f2c3148a
esudalaimadan <38071735+esudalaimadan@users.noreply.github.com>
2018-12-19 17:27:22 +0530

    Add placeholder text for Post Type dropdown
    Fix #571

--- include/Core/Base/Mixin/Renderer.php
+++ include/Core/Base/Mixin/Renderer.php
@@ -79,7 +79,7 @@ abstract class Renderer extends Fetcher {
 		?>
 		<tr>
 			<td scope="row" colspan="2">
-				<select class="enhanced-post-types-with-status" multiple="multiple" name="smbd_<?php echo esc_attr( $this->field_slug ); ?>[]">
+				<select class="enhanced-post-types-with-status" multiple="multiple" data-placeholder="Select Post Type" name="smbd_<?php echo esc_attr( $this->field_slug ); ?>[]">
 				<?php foreach ( $post_types_by_status as $post_type => $all_status ) : ?>
 					<optgroup label="<?php echo esc_html( $post_type ); ?>">
 					<?php foreach ( $all_status as $status_key => $status_value ) : ?>
