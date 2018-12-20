816d866baddc6aa2f957aad3c8df6a2b251ef014
Sudar Muthu <sudar@sudarmuthu.com>
2018-12-20 12:07:14 +0530

    Add page parameter to before and after enqueue scripts actions

--- include/Core/Base/BaseDeletePage.php
+++ include/Core/Base/BaseDeletePage.php
@@ -83,9 +83,12 @@ abstract class BaseDeletePage extends BasePage {
 		 *
 		 * This action is primarily for registering or deregistering additional scripts or styles.
 		 *
+		 * @param \BulkWP\BulkDelete\Core\Base\BaseDeletePage The current page.
+		 *
 		 * @since 5.5.1
+		 * @since 6.0.0 Added $page parameter.
 		 */
-		do_action( 'bd_before_admin_enqueue_scripts' );
+		do_action( 'bd_before_admin_enqueue_scripts', $this );
 
 		wp_enqueue_script(
 			'jquery-ui-timepicker-addon',
@@ -135,9 +138,12 @@ abstract class BaseDeletePage extends BasePage {
 		 *
 		 * This action is primarily for registering additional scripts or styles.
 		 *
+		 * @param \BulkWP\BulkDelete\Core\Base\BaseDeletePage The current page.
+		 *
 		 * @since 5.5.1
+		 * @since 6.0.0 Added $page parameter.
 		 */
-		do_action( 'bd_after_admin_enqueue_scripts' );
+		do_action( 'bd_after_admin_enqueue_scripts', $this );
 	}
 
 	/**
