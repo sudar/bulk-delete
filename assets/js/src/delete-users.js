ed160041e07deb34b703387f475b75ae98336a5b
Sudar Muthu <sudar@sudarmuthu.com>
2019-01-12 19:47:42 +0530

    Fix whitespace issues

--- assets/js/src/delete-users.js
+++ assets/js/src/delete-users.js
@@ -1,5 +1,5 @@
 /**
- * JavaScript for delete users module of Bulk Delete Plugin.
+ * JavaScript for Delete Users module of Bulk Delete Plugin.
  *
  * @author Sudar <https://bulkwp.com>
  * @since 6.0.0
@@ -7,27 +7,28 @@
 
 /*global jQuery, BulkWP*/
 jQuery( document ).ready( function () {
-    var reassignSelectBoxes = jQuery(".reassign-user");
-    var contentDeleteRadios = jQuery(".post-reassign");
+	var reassignSelectBoxes = jQuery( ".reassign-user" ),
+		contentDeleteRadios = jQuery( ".post-reassign" );
 
-    reassignSelectBoxes.each( function() {
-        jQuery(this).attr('disabled', 'true');
-    });
+	reassignSelectBoxes.each( function () {
+		jQuery( this ).attr( 'disabled', 'true' );
+	} );
 
-    contentDeleteRadios.change( function () {
-        var reassignSelectBox = jQuery(this).parents('tr').find('.reassign-user');
-        if( "true" === jQuery(this).val() ){
-            reassignSelectBox.removeAttr('disabled');
-        } else {
-            reassignSelectBox.attr('disabled', 'true');
+	contentDeleteRadios.change( function () {
+		var reassignSelectBox = jQuery( this ).parents( 'tr' ).find( '.reassign-user' );
+
+		if ( "true" === jQuery( this ).val() ) {
+			reassignSelectBox.removeAttr( 'disabled' );
+		} else {
+			reassignSelectBox.attr( 'disabled', 'true' );
 		}
-	});
-});
+	} );
+} );
 
-BulkWP.validateUserMeta = function() {
-    return (jQuery('#smbd_u_meta_value').val() !== '');
+BulkWP.validateUserMeta = function () {
+	return (jQuery( '#smbd_u_meta_value' ).val() !== '');
 };
 
-BulkWP.validateUserRole = function(that) {
-    return (null !== jQuery(that).parent().prev().find(".enhanced-role-dropdown").val());
+BulkWP.validateUserRole = function ( that ) {
+	return (null !== jQuery( that ).parent().prev().find( ".enhanced-role-dropdown" ).val());
 };
