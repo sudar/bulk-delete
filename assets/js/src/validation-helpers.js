d616cac8f0f737a7fba5ddd408f80530e2e5fcce
Sudar Muthu <sudar@sudarmuthu.com>
2019-01-26 23:12:58 +0530

    Move validaiton helper functions to a seperate file

---
+++ assets/js/src/validation-helpers.js
@@ -0,0 +1,16 @@
+/**
+ * Common Validation helper functions.
+ *
+ * @since 6.0.0
+ */
+
+/*global BulkWP*/
+
+/**
+ * No need to validate anything.
+ *
+ * @returns {boolean} Returns true always.
+ */
+BulkWP.noValidation = function() {
+	return true;
+};
