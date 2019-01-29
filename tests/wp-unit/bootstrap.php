645a230d822f7671530d704383c7fa11410fbcf7
Sudar Muthu <sudar@sudarmuthu.com>
2019-01-30 00:06:17 +0530

    Downgrade PHPUnit to v6.x

--- tests/wp-unit/bootstrap.php
+++ tests/wp-unit/bootstrap.php
@@ -3,11 +3,18 @@
  * PHPUnit bootstrap file.
  */
 
-define( 'BD_TEST_PLUGIN_BASE_FILE', dirname( dirname( __FILE__ ) ) . '/../bulk-delete.php' );
-
-// Load BulkWP test tools.
-if ( ! file_exists( dirname( dirname( __FILE__ ) ) . '/../vendor/sudar/wp-plugin-test-tools/src/Tests/WPCore/bootstrap.php' ) ) {
+// Load bootstrap file from BulkWP test tools.
+if ( ! file_exists( dirname( dirname( __FILE__ ) ) . '/../vendor/autoload.php' ) ) {
 	echo 'Could not find BulkWP Test tools. Have you run composer install?' . PHP_EOL;
 	exit( 1 );
 }
-require_once dirname( dirname( __FILE__ ) ) . '/../vendor/sudar/wp-plugin-test-tools/src/Tests/WPCore/bootstrap.php';
+require_once dirname( dirname( __FILE__ ) ) . '/../vendor/autoload.php';
+
+/**
+ * Manually load the plugin being tested.
+ */
+\BulkWP\Tests\WPCore\load_plugins_for_testing(
+	array(
+		dirname( dirname( __FILE__ ) ) . '/../bulk-delete.php',
+	)
+);
