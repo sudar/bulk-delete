5f54e734547c6eb344044573fd8ddc5b55d2a375
Sudar Muthu <sudar@sudarmuthu.com>
2019-01-29 07:48:29 +0530

    Pull bootstrap file from wp-plugin-test-tools library

--- tests/wp-unit/bootstrap.php
+++ tests/wp-unit/bootstrap.php
@@ -1,37 +1,13 @@
 <?php
 /**
  * PHPUnit bootstrap file.
- *
- * @package Bulk_Delete
  */
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
 
-if ( ! $_tests_dir ) {
-	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
-}
-
-if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
-	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL;
-	exit( 1 );
-}
-
-// Give access to tests_add_filter() function.
-require_once $_tests_dir . '/includes/functions.php';
-
-/**
- * Manually load the plugin being tested.
- */
-function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/../bulk-delete.php';
-}
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
-
-// Start up the WP testing environment.
-require $_tests_dir . '/includes/bootstrap.php';
+define( 'BD_TEST_PLUGIN_BASE_FILE', dirname( dirname( __FILE__ ) ) . '/../bulk-delete.php' );
 
 // Load BulkWP test tools.
-if ( ! file_exists( '../bulk-delete/vendor/sudar/wp-plugin-test-tools/src/Tests/WPCore/WPCoreUnitTestCase.php' ) ) {
+if ( ! file_exists( dirname( dirname( __FILE__ ) ) . '/../vendor/sudar/wp-plugin-test-tools/src/Tests/WPCore/bootstrap.php' ) ) {
 	echo 'Could not find BulkWP Test tools. Have you run composer install?' . PHP_EOL;
 	exit( 1 );
 }
-require_once '../bulk-delete/vendor/sudar/wp-plugin-test-tools/src/Tests/WPCore/WPCoreUnitTestCase.php';
+require_once dirname( dirname( __FILE__ ) ) . '/../vendor/sudar/wp-plugin-test-tools/src/Tests/WPCore/bootstrap.php';
