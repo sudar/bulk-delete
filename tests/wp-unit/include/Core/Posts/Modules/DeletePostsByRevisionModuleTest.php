23faa79042295f9839070bfb6868d7e184fab3b6
esudalaimadan <ezhil.madan@gmail.com>
2019-07-24 13:57:44 +0530

    Enhance delete revisions module by adding date and limit filter.
    Fix #629

--- tests/wp-unit/include/Core/Posts/Modules/DeletePostsByRevisionModuleTest.php
+++ tests/wp-unit/include/Core/Posts/Modules/DeletePostsByRevisionModuleTest.php
@@ -52,7 +52,10 @@ class DeletePostsByRevisionModuleTest extends WPCoreUnitTestCase {
 		wp_update_post( $revision_post_2 );
 
 		$delete_options = array(
-			'revisions' => 'revisions',
+			'revisions'    => 'revisions',
+			'limit_to'     => 0,
+			'restrict'     => false,
+			'force_delete' => true,
 		);
 
 		$posts_deleted = $this->module->delete( $delete_options );
@@ -67,7 +70,8 @@ class DeletePostsByRevisionModuleTest extends WPCoreUnitTestCase {
 	 */
 	public function test_that_post_revisions_can_be_deleted_from_multiple_post() {
 		$post_ids = $this->factory->post->create_many(
-			10, array(
+			10,
+			array(
 				'post_type' => 'post',
 			)
 		);
@@ -83,7 +87,10 @@ class DeletePostsByRevisionModuleTest extends WPCoreUnitTestCase {
 		}
 
 		$delete_options = array(
-			'revisions' => 'revisions',
+			'revisions'    => 'revisions',
+			'limit_to'     => 0,
+			'restrict'     => false,
+			'force_delete' => true,
 		);
 
 		$posts_deleted = $this->module->delete( $delete_options );
@@ -94,4 +101,147 @@ class DeletePostsByRevisionModuleTest extends WPCoreUnitTestCase {
 			$this->assertEquals( $post->ID, $post_id );
 		}
 	}
+
+	/**
+	 * Data provider to test revisions can be deleted with date filter.
+	 */
+	public function provide_data_to_test_revisions_deletion_with_date_filter() {
+		return array(
+			// (+ve Case) Deletes all/few revisions that are older than x days.
+			array(
+				array(
+					array(
+						'no_of_posts' => 10,
+						'post_args'   => array(
+							'post_type'   => 'revision',
+							'post_status' => 'inherit',
+							'post_date'   => date( 'Y-m-d H:i:s', strtotime( '-4 day' ) ),
+						),
+					),
+					array(
+						'no_of_posts' => 5,
+						'post_args'   => array(
+							'post_type'   => 'revision',
+							'post_status' => 'inherit',
+							'post_date'   => date( 'Y-m-d H:i:s', strtotime( '-1 day' ) ),
+						),
+					),
+				),
+				array(
+					'revisions'    => 'revisions',
+					'limit_to'     => 0,
+					'restrict'     => true,
+					'date_op'      => 'before',
+					'days'         => 2,
+					'force_delete' => true,
+				),
+				array(
+					'deleted_posts'   => 10,
+					'available_posts' => 5,
+				),
+			),
+			// (+ve Case) Deletes all/few revisions in the last x days.
+			array(
+				array(
+					array(
+						'no_of_posts' => 10,
+						'post_args'   => array(
+							'post_type'   => 'revision',
+							'post_status' => 'inherit',
+							'post_date'   => date( 'Y-m-d H:i:s', strtotime( '-4 day' ) ),
+						),
+					),
+					array(
+						'no_of_posts' => 5,
+						'post_args'   => array(
+							'post_type'   => 'revision',
+							'post_status' => 'inherit',
+							'post_date'   => date( 'Y-m-d H:i:s', strtotime( '-1 day' ) ),
+						),
+					),
+				),
+				array(
+					'revisions'    => 'revisions',
+					'limit_to'     => 0,
+					'restrict'     => true,
+					'date_op'      => 'after',
+					'days'         => 2,
+					'force_delete' => true,
+				),
+				array(
+					'deleted_posts'   => 5,
+					'available_posts' => 10,
+				),
+			),
+		);
+	}
+
+	/**
+	 * Data provider to test revisions can be deleted with limit filter.
+	 */
+	public function provide_data_to_test_revisions_deletion_with_limit_filter() {
+		return array(
+			// (+ve Case) Deletes all/few revisions with limit filter.
+			array(
+				array(
+					array(
+						'no_of_posts' => 10,
+						'post_args'   => array(
+							'post_type'   => 'revision',
+							'post_status' => 'inherit',
+							'post_date'   => date( 'Y-m-d H:i:s', strtotime( '-4 day' ) ),
+						),
+					),
+					array(
+						'no_of_posts' => 5,
+						'post_args'   => array(
+							'post_type'   => 'revision',
+							'post_status' => 'inherit',
+							'post_date'   => date( 'Y-m-d H:i:s', strtotime( '-1 day' ) ),
+						),
+					),
+					array(
+						'no_of_posts' => 20,
+						'post_args'   => array(
+							'post_type'   => 'revision',
+							'post_status' => 'inherit',
+						),
+					),
+				),
+				array(
+					'revisions'    => 'revisions',
+					'limit_to'     => 15,
+					'restrict'     => false,
+					'force_delete' => true,
+				),
+				array(
+					'deleted_posts'   => 15,
+					'available_posts' => 20,
+				),
+			),
+		);
+	}
+
+	/**
+	 * Test deletion of revisions with date and limit filters.
+	 *
+	 * @param array $setup     Create revisions.
+	 * @param array $operation Possible operations.
+	 * @param array $expected  Expected output.
+	 *
+	 * @dataProvider provide_data_to_test_revisions_deletion_with_date_filter
+	 * @dataProvider provide_data_to_test_revisions_deletion_with_limit_filter
+	 */
+	public function test_that_post_revisions_can_be_deleted( $setup, $operation, $expected ) {
+		foreach ( $setup as $element ) {
+			$this->factory->post->create_many( $element['no_of_posts'], $element['post_args'] );
+		}
+
+		$posts_deleted = $this->module->delete( $operation );
+		$this->assertEquals( $expected['deleted_posts'], $posts_deleted );
+
+		$available_posts = $this->get_posts_by_status( 'inherit', 'revision' );
+		$this->assertEquals( $expected['available_posts'], count( $available_posts ) );
+	}
+
 }
