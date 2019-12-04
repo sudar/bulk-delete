154f1a3366bfdec7a76bd6934e42ef0c2856f192
esudalaimadan <ezhil.madan@gmail.com>
2019-12-04 11:41:35 +0530

    Remove unused JS code
    Fix #691

--- assets/js/src/select2.js
+++ assets/js/src/select2.js
@@ -2,7 +2,7 @@
  * Add select2 functionality..
  */
 
-/*global ajaxurl*/
+/*global*/
 jQuery( document ).ready( function () {
 	/**
 	 * Normal select2.
@@ -28,40 +28,4 @@ jQuery( document ).ready( function () {
 			);
 		}
 	});
-
-	/**
-	 * Enable AJAX for Taxonomy Select2.
-	 *
-	 * TODO: See if this is used anywhere. Else remove it.
-	 */
-	jQuery( '.select2-taxonomy-ajax' ).select2( {
-		ajax: {
-			url: ajaxurl,
-			dataType: 'json',
-			delay: 250,
-			data: function ( params ) {
-				return {
-					q: params.term,
-					taxonomy: jQuery( this ).attr( 'data-taxonomy' ),
-					action: 'bd_load_taxonomy_term'
-				};
-			},
-			processResults: function ( data ) {
-				var options = [];
-
-				if ( data ) {
-					jQuery.each( data, function ( index, dataPair ) {
-						options.push( { id: dataPair[ 0 ], text: dataPair[ 1 ] } );
-					} );
-				}
-
-				return {
-					results: options
-				};
-			},
-			cache: true
-		},
-		minimumInputLength: 2, // the minimum of symbols to input before perform a search.
-		width: '300px'
-	} );
 } );
